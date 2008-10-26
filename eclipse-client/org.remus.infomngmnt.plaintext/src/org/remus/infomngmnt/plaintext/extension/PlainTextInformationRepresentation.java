ba13be61df2d6045494d7db810b5d39525b975f6
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2008-10-26 23:12:13 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/extension/PlainTextInformationRepresentation.java
+++ eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/extension/PlainTextInformationRepresentation.java
@@ -13,10 +13,16 @@
 package org.remus.infomngmnt.plaintext.extension;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFileState;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
 
+import org.remus.infomngmnt.InfomngmntPackage;
 import org.remus.infomngmnt.core.extension.AbstractInformationRepresentation;
+import org.remus.infomngmnt.core.model.EditingUtil;
+
+import org.apache.commons.lang.StringEscapeUtils;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
@@ -29,14 +35,19 @@ AbstractInformationRepresentation {
 	@Override
 	public void handlePostBuild(IFile derivedFile, IProgressMonitor monitor)
 	throws CoreException {
-		// TODO Auto-generated method stub
+		// do nothing
 
 	}
 
 	@Override
 	public String handleHtmlGeneration(IProgressMonitor monitor)
 	throws CoreException {
-		return getValue().getStringValue();
+		String returnValue = StringEscapeUtils.escapeHtml(getValue().getStringValue());
+		returnValue = returnValue.replaceAll("\\r\\n", "<br />");
+		IFileState previousVersion2 = getPreviousVersion();
+		EObject objectFromUri = EditingUtil.getInstance().getObjectFromUri(previousVersion2.getFullPath(), InfomngmntPackage.Literals.ABSTRACT_INFORMATION_UNIT,false);
+		returnValue = returnValue.replaceAll("[\\t\\f]", "&nbsp;");
+		return returnValue;
 	}
 
 	@Override

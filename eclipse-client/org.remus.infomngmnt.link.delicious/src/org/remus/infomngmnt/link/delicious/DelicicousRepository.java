7cebce632aa25048a990ddb388c4c8d15e89922b
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-01-03 11:25:21 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.link.delicious/src/org/remus/infomngmnt/link/delicious/DelicicousRepository.java
+++ eclipse-client/org.remus.infomngmnt.link.delicious/src/org/remus/infomngmnt/link/delicious/DelicicousRepository.java
@@ -23,6 +23,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
 
 import del.icio.us.Delicious;
 import del.icio.us.DeliciousConstants;
@@ -47,6 +48,15 @@ public class DelicicousRepository extends AbstractExtensionRepository {
 	private final PropertyChangeListener credentialsMovedListener = new PropertyChangeListener() {
 		public void propertyChange(final PropertyChangeEvent evt) {
 			reset();
+		}
+	};
+	
+	final ISchedulingRule mutexRule = new ISchedulingRule() {
+		public boolean isConflicting(final ISchedulingRule rule) {
+			return rule == DelicicousRepository.this.mutexRule;
+		}
+		public boolean contains(final ISchedulingRule rule) {
+			return rule == DelicicousRepository.this.mutexRule;
 		}
 	};
 
@@ -121,6 +131,12 @@ public class DelicicousRepository extends AbstractExtensionRepository {
 	public void reset() {
 		this.api = null;
 		getCredentialProvider().removePropertyChangeListener(this.credentialsMovedListener);
+	}
+
+	
+
+	public ISchedulingRule getRule() {
+		return this.mutexRule;
 	}
 
 

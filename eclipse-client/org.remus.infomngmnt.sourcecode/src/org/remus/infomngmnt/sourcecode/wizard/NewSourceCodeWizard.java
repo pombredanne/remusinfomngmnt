5d52acecd2edf96362fb5ae4cdce3637650486c4
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-01-05 14:13:23 +0000

    remote integration

--- eclipse-client/org.remus.infomngmnt.sourcecode/src/org/remus/infomngmnt/sourcecode/wizard/NewSourceCodeWizard.java
+++ eclipse-client/org.remus.infomngmnt.sourcecode/src/org/remus/infomngmnt/sourcecode/wizard/NewSourceCodeWizard.java
@@ -16,7 +16,6 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbench;
 
 import org.remus.infomngmnt.Category;
-import org.remus.infomngmnt.InfomngmntFactory;
 import org.remus.infomngmnt.InformationUnit;
 import org.remus.infomngmnt.InformationUnitListItem;
 import org.remus.infomngmnt.RuleValue;
@@ -38,18 +37,9 @@ public class NewSourceCodeWizard extends NewInfoObjectWizard {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	protected InformationUnit createNewInformationUnit() {
-		InformationUnit informationUnit = super.createNewInformationUnit();
-		informationUnit.setType(SourceCodePlugin.TYPE_ID);
+	
 
-		InformationUnit sourceCodeType = InfomngmntFactory.eINSTANCE.createInformationUnit();
-		sourceCodeType.setType(SourceCodePlugin.SRCTYPE_NAME);
-		informationUnit.getChildValues().add(sourceCodeType);
-		return informationUnit;
-	}
-
-	public void setDefaults(Object value, RuleValue ruleValue) {
+	public void setDefaults(final Object value, final RuleValue ruleValue) {
 		this.newElement.setStringValue(String.valueOf(value));
 		InformationUnit childByType = InformationUtil.getChildByType(
 				ruleValue, AbstractCreationPreferencePage.NODENAME_PREDEFINED_CATEGORY);
@@ -68,7 +58,7 @@ public class NewSourceCodeWizard extends NewInfoObjectWizard {
 	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
 	 */
 	@Override
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
+	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
 		Object firstElement = selection.getFirstElement();
 		if (firstElement instanceof Category) {
 			this.page1 = new GeneralSourcePage((Category) firstElement);
@@ -78,6 +68,13 @@ public class NewSourceCodeWizard extends NewInfoObjectWizard {
 			this.page1 = new GeneralSourcePage((Category)null);
 		}
 
+	}
+
+
+
+	@Override
+	protected String getInfoTypeId() {
+		return SourceCodePlugin.TYPE_ID;
 	}
 
 }

dfd121aa83b06b0354cc64e9d48eae93a2ff97cb
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-01-08 18:46:35 +0000

    minor changes

--- trunk/eclipse-client/org.remus.infomngmnt.link.delicious/src/org/remus/infomngmnt/link/delicious/actions/CheckoutTagAction.java
+++ trunk/eclipse-client/org.remus.infomngmnt.link.delicious/src/org/remus/infomngmnt/link/delicious/actions/CheckoutTagAction.java
@@ -12,25 +12,75 @@
 
 package org.remus.infomngmnt.link.delicious.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.BaseSelectionListenerAction;
+
+import org.remus.infomngmnt.ChangeSet;
+import org.remus.infomngmnt.ChangeSetItem;
+import org.remus.infomngmnt.InfomngmntFactory;
+import org.remus.infomngmnt.RemoteContainer;
+import org.remus.infomngmnt.RemoteObject;
+import org.remus.infomngmnt.common.ui.UIUtil;
+import org.remus.infomngmnt.core.remote.IRepository;
+import org.remus.infomngmnt.core.services.IRepositoryExtensionService;
+import org.remus.infomngmnt.ui.UIPlugin;
+import org.remus.infomngmnt.ui.remote.SynchronizationWizard;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
  */
 public class CheckoutTagAction extends BaseSelectionListenerAction {
-	
+
 	public CheckoutTagAction() {
 		super("Categories");
 		setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJ_FOLDER));
 	}
-	
-	
+
+
+
 	@Override
 	public void run() {
-		
+		ChangeSet createChangeSet = InfomngmntFactory.eINSTANCE.createChangeSet();
+		List list = getStructuredSelection().toList();
+		String repositoryId = null;
+		List<RemoteContainer> remoteContainers = new ArrayList<RemoteContainer>();
+		for (Object object : list) {
+			RemoteContainer remoteContainer = null;
+			if (repositoryId == null) {
+				repositoryId = ((RemoteObject) object).getRepositoryTypeId();
+			}
+			if (object instanceof RemoteContainer) {
+				remoteContainer = (RemoteContainer) object;
+			} else if (object instanceof RemoteObject) {
+				remoteContainer = (RemoteContainer) ((RemoteObject) object).eContainer();
+			}
+			if (!remoteContainers.contains(remoteContainer)) {
+				remoteContainers.add(remoteContainer);
+			}
+		}
+
+
+		for (RemoteContainer remoteContainer : remoteContainers) {
+			ChangeSetItem createChangeSetItem = InfomngmntFactory.eINSTANCE.createChangeSetItem();
+			createChangeSetItem.setRemoteOriginalObject(remoteContainer);
+			createChangeSet.getChangeSetItems().add(createChangeSetItem);
+		}
+
+		IRepository itemById = UIPlugin.getDefault().getService(IRepositoryExtensionService.class).getItemById(repositoryId);
+		itemById.applyChangeSet(createChangeSet);
+
+		SynchronizationWizard synchronizationWizard = new SynchronizationWizard();
+		synchronizationWizard.init(createChangeSet);
+		WizardDialog wz = new WizardDialog(UIUtil.getPrimaryWindow().getShell(), synchronizationWizard);
+		wz.open();
+
 		super.run();
 	}
+
 
 }

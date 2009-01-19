25b8f53d59664700518027c3082dff628c43560b
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-01-19 08:38:10 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/handler/ReplaceHandler.java
+++ eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/handler/ReplaceHandler.java
@@ -12,18 +12,32 @@
 
 package org.remus.infomngmnt.ui.handler;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import org.remus.infomngmnt.Category;
+import org.remus.infomngmnt.ChangeSet;
+import org.remus.infomngmnt.RemoteContainer;
+import org.remus.infomngmnt.RemoteObject;
+import org.remus.infomngmnt.RemoteRepository;
 import org.remus.infomngmnt.SynchronizableObject;
 import org.remus.infomngmnt.common.core.util.CollectionFilter;
 import org.remus.infomngmnt.common.core.util.CollectionUtils;
 import org.remus.infomngmnt.common.core.util.ModelUtil;
+import org.remus.infomngmnt.core.commands.CommandFactory;
+import org.remus.infomngmnt.core.model.EditingUtil;
+import org.remus.infomngmnt.core.services.IRepositoryService;
+import org.remus.infomngmnt.core.sync.ChangeSetExecutor;
+import org.remus.infomngmnt.core.sync.ChangeSetManager;
+import org.remus.infomngmnt.ui.UIPlugin;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
@@ -45,10 +59,31 @@ public class ReplaceHandler extends AbstractRemoteHandler {
 				}
 			});
 			for (SynchronizableObject synchronizableObject : filteredList) {
-				
+				String repositoryId = synchronizableObject.getSynchronizationMetaData().getRepositoryId();
+				RemoteRepository localRepository = UIPlugin.getDefault().getService(IRepositoryService.class).getRepositoryById(repositoryId);
+				RemoteObject remoteObject = localRepository.getRepositoryImplementation()
+					.getRemoteObjectBySynchronizableObject(synchronizableObject);
+				if (remoteObject == null) {
+					/*
+					 *  something went wrong. Show a notification here.
+					 *  It's the best to do a synchronization at this
+					 *  time.
+					 */
+					
+				}
+				else if (remoteObject instanceof RemoteContainer && synchronizableObject instanceof Category) {
+					ChangeSet changeSet = new ChangeSetManager().createCheckOutChangeSet(Collections.<RemoteContainer> singletonList((RemoteContainer)remoteObject));
+					changeSet.setTargetCategory((Category) synchronizableObject.eContainer());
+					EditingDomain navigationEditingDomain = EditingUtil.getInstance().getNavigationEditingDomain();
+					Command deleteCategory = CommandFactory.DELETE_CATEGORY((Category) synchronizableObject, navigationEditingDomain);
+					navigationEditingDomain.getCommandStack().execute(deleteCategory);
+					ChangeSetExecutor changeSetExecutor = new ChangeSetExecutor();
+					changeSetExecutor.setChangeSet(changeSet);
+					//changeSetExecutor.performCheckout(ownedElements)
+					
 			}
 		}
-		
+		}
 		return null;
 	}
 
--- eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/remote/SynchronizationWizard.java
+++ eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/remote/SynchronizationWizard.java
@@ -12,29 +12,14 @@
 
 package org.remus.infomngmnt.ui.remote;
 
-import java.util.List;
-
-import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.compare.diff.metamodel.AddModelElement;
 import org.eclipse.emf.compare.diff.metamodel.DiffElement;
 import org.eclipse.emf.compare.diff.metamodel.DiffModel;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.widgets.Composite;
 
-import org.remus.infomngmnt.Category;
 import org.remus.infomngmnt.ChangeSet;
-import org.remus.infomngmnt.ChangeSetItem;
-import org.remus.infomngmnt.InfomngmntPackage;
-import org.remus.infomngmnt.InformationUnit;
-import org.remus.infomngmnt.InformationUnitListItem;
-import org.remus.infomngmnt.common.core.util.ModelUtil;
-import org.remus.infomngmnt.core.extension.ISaveParticipant;
-import org.remus.infomngmnt.core.model.CategoryUtil;
-import org.remus.infomngmnt.core.model.EditingUtil;
-import org.remus.infomngmnt.core.services.ISaveParticipantExtensionService;
-import org.remus.infomngmnt.ui.UIPlugin;
+import org.remus.infomngmnt.core.sync.ChangeSetExecutor;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
@@ -42,7 +27,7 @@ import org.remus.infomngmnt.ui.UIPlugin;
 public class SynchronizationWizard extends Wizard {
 
 	private ChangeSetWizardPage page1;
-	private ChangeSet changeSet;
+	private final ChangeSetExecutor changeSetExecutor;
 
 	public static final int CHECKOUTMODE = 1; 
 	public static final int SHAREMODE = 2; 
@@ -51,17 +36,19 @@ public class SynchronizationWizard extends Wizard {
 
 	public SynchronizationWizard(final int mode) {
 		this.mode = mode;
+		this.changeSetExecutor = new ChangeSetExecutor();
 		setNeedsProgressMonitor(true);
+		
 	}
 
 	@Override
 	public void addPages() {
-		addPage(this.page1 = new ChangeSetWizardPage(this.changeSet));
+		addPage(this.page1 = new ChangeSetWizardPage(this.changeSetExecutor.getChangeSet()));
 
 	}
 
 	public void init(final ChangeSet changeSet) {
-		this.changeSet = changeSet;
+		this.changeSetExecutor.setChangeSet(changeSet);
 	}
 
 	@Override
@@ -80,7 +67,7 @@ public class SynchronizationWizard extends Wizard {
 		EList<DiffElement> ownedElements = diffModel.getOwnedElements();
 		switch (this.mode) {
 		case CHECKOUTMODE:
-			performCheckout(ownedElements);
+			this.changeSetExecutor.performCheckout(ownedElements);
 			break;
 		default:
 			break;
@@ -88,35 +75,6 @@ public class SynchronizationWizard extends Wizard {
 		return true;
 	}
 
-	private void performCheckout(final EList<DiffElement> ownedElements) {
-		for (DiffElement diffElement : ownedElements) {
-			if (diffElement instanceof AddModelElement) {
-				EObject rightElement = ((AddModelElement) diffElement).getRightElement();
-				EList<ChangeSetItem> changeSetItems = this.changeSet.getChangeSetItems();
-				for (ChangeSetItem changeSetItem : changeSetItems) {
-					if (changeSetItem.getRemoteConvertedContainer().getId().equals(((Category) rightElement).getId())) {
-						Category remoteConvertedContainer = changeSetItem.getRemoteConvertedContainer();
-						this.changeSet.getTargetCategory().getChildren().add(remoteConvertedContainer);
-						List<InformationUnitListItem> allChildren = ModelUtil.getAllChildren(remoteConvertedContainer, InfomngmntPackage.Literals.INFORMATION_UNIT_LIST_ITEM);
-						for (InformationUnitListItem informationUnitListItem : allChildren) {
-							InformationUnit informationUnit2 = changeSetItem.getRemoteFullObjectMap().get(informationUnitListItem);
-							if (informationUnit2 == null) {
-								InformationUnit informationUnit = this.changeSet.getRepository().getRepositoryImplementation().getFullObject(informationUnitListItem);
-								informationUnit.setId(informationUnitListItem.getId());
-								informationUnit.setType(informationUnitListItem.getId());
-							}
-							IFile newFile = CategoryUtil.getProjectByCategory(remoteConvertedContainer).getFile(informationUnitListItem.getId() + ".info");
-							EditingUtil.getInstance().saveObjectToResource(newFile, informationUnit2);
-							informationUnitListItem.setWorkspacePath(newFile.getFullPath().toOSString());
-							UIPlugin.getDefault().getService(ISaveParticipantExtensionService.class)
-							.fireEvent(ISaveParticipant.CREATED, informationUnit2);
-						}
-					}
-				}
-			}
-			performCheckout(diffElement.getSubDiffElements());
-		}
-	}
 
 
 }

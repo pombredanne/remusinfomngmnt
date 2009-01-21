3afd38e3987e693a74629b5305c11f60f36f7886
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-01-21 14:06:47 +0000

    minor changes

--- trunk/eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/extension/PlainTextInformationRepresentation.java
+++ trunk/eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/extension/PlainTextInformationRepresentation.java
@@ -12,13 +12,23 @@
 
 package org.remus.infomngmnt.plaintext.extension;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 
+import org.remus.infomngmnt.common.core.streams.StreamCloser;
 import org.remus.infomngmnt.core.extension.AbstractInformationRepresentation;
-
-import org.apache.commons.lang.StringEscapeUtils;
+import org.remus.infomngmnt.core.model.StatusCreator;
+import org.remus.infomngmnt.jslib.rendering.FreemarkerRenderer;
+import org.remus.infomngmnt.plaintext.Activator;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
@@ -29,37 +39,51 @@ AbstractInformationRepresentation {
 
 
 	@Override
-	public void handlePostBuild(IFile derivedFile, IProgressMonitor monitor)
+	public void handlePostBuild(final IFile derivedFile, final IProgressMonitor monitor)
 	throws CoreException {
 		// do nothing
 
 	}
 
 	@Override
-	public String handleHtmlGeneration(IProgressMonitor monitor)
+	public InputStream handleHtmlGeneration(final IProgressMonitor monitor)
 	throws CoreException {
-		String returnValue = StringEscapeUtils.escapeHtml(getValue().getStringValue());
-		if (returnValue != null) {
-			returnValue = returnValue.replaceAll("\\r\\n", "<br />");
-			returnValue = returnValue.replaceAll("[\\t\\f]", "&nbsp;");
+		ByteArrayOutputStream returnValue = new ByteArrayOutputStream();
+		InputStream templateIs = null;
+		InputStream contentsIs = getFile().getContents();
+		try {
+			templateIs = FileLocator.openStream(
+					Platform.getBundle(Activator.PLUGIN_ID), 
+					new Path("template/htmlserialization.flt"), false);
+			FreemarkerRenderer.getInstance().process(
+					Activator.PLUGIN_ID,
+					templateIs,
+					contentsIs,
+					returnValue, null);
+		} catch (IOException e) {
+			throw new CoreException(StatusCreator.newStatus(
+					"Error reading locations",e));
+		} finally {
+			StreamCloser.closeStreams(templateIs, contentsIs);
 		}
-		return returnValue;
+		return new ByteArrayInputStream(returnValue.toByteArray());
 	}
 
 	@Override
-	public String getAdditionalsForIndexing(IProgressMonitor monitor)
+	public String getAdditionalsForIndexing(final IProgressMonitor monitor)
 	throws CoreException {
 		return null;
 	}
 
 	@Override
-	public String getBodyForIndexing(IProgressMonitor monitor)
+	public String getBodyForIndexing(final IProgressMonitor monitor)
 	throws CoreException {
+
 		return getValue().getStringValue();
 	}
 
 	@Override
-	public String getTitleForIndexing(IProgressMonitor monitor)
+	public String getTitleForIndexing(final IProgressMonitor monitor)
 	throws CoreException {
 		return getValue().getLabel();
 	}

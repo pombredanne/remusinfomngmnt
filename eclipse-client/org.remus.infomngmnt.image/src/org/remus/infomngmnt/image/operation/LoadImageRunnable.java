1e59431022f1027bc4dcaacedea76e36c0239737
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-01-28 18:09:28 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.image/src/org/remus/infomngmnt/image/operation/LoadImageRunnable.java
+++ eclipse-client/org.remus.infomngmnt.image/src/org/remus/infomngmnt/image/operation/LoadImageRunnable.java
@@ -44,6 +44,7 @@ import com.drew.metadata.exif.ExifDirectory;
 import org.remus.infomngmnt.InfomngmntFactory;
 import org.remus.infomngmnt.InfomngmntPackage;
 import org.remus.infomngmnt.InformationUnit;
+import org.remus.infomngmnt.common.core.streams.FileUtil;
 import org.remus.infomngmnt.common.ui.UIUtil;
 import org.remus.infomngmnt.core.model.EditingUtil;
 import org.remus.infomngmnt.core.model.InformationUtil;
@@ -92,7 +93,7 @@ public class LoadImageRunnable extends CancelableRunnable {
 						this.domain,
 						this.rawImageDataNode,
 						InfomngmntPackage.Literals.INFORMATION_UNIT__BINARY_VALUE,
-						getBytesFromFile(this.file, monitor)));
+						FileUtil.getBytesFromFile(this.file, monitor)));
 				boolean isJpgeg = Pattern.compile("^.*\\.jpe?g$", Pattern.CASE_INSENSITIVE).matcher(this.imagePath).matches();
 				Metadata metadata = null;
 				List<InformationUnit> exifData = new ArrayList<InformationUnit>();
@@ -187,42 +188,7 @@ public class LoadImageRunnable extends CancelableRunnable {
 
 	
 	
-	 // Returns the contents of the file in a byte array.
-    public static byte[] getBytesFromFile(final File file, final IProgressMonitor monitor) throws IOException {
-        InputStream is = new FileInputStream(file);
-    
-        // Get the size of the file
-        long length = file.length();
-    
-        // You cannot create an array using a long type.
-        // It needs to be an int type.
-        // Before converting to an int type, check
-        // to ensure that file is not larger than Integer.MAX_VALUE.
-        if (length > Integer.MAX_VALUE) {
-            // File is too large
-        }
-    
-        // Create the byte array to hold the data
-        byte[] bytes = new byte[(int)length];
-    
-        // Read in the bytes
-        int offset = 0;
-        int numRead = 0;
-        while (offset < bytes.length
-               && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
-            offset += numRead;
-            monitor.worked(1);
-        }
-    
-        // Ensure all the bytes have been read in
-        if (offset < bytes.length) {
-            throw new IOException("Could not completely read file "+file.getName());
-        }
-    
-        // Close the input stream and return bytes
-        is.close();
-        return bytes;
-    }
+	
 
 	public void setImageNode(final InformationUnit infoUnit) {
 		this.infoUnit = infoUnit;

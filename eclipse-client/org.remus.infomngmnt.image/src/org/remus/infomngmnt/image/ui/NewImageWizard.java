0fca8127208526a4960b18a755b556830f3a57d8
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-02-15 20:33:13 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.image/src/org/remus/infomngmnt/image/ui/NewImageWizard.java
+++ eclipse-client/org.remus.infomngmnt.image/src/org/remus/infomngmnt/image/ui/NewImageWizard.java
@@ -12,12 +12,22 @@
 
 package org.remus.infomngmnt.image.ui;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.ui.IWorkbench;
 
 import org.remus.infomngmnt.Category;
+import org.remus.infomngmnt.InformationUnit;
 import org.remus.infomngmnt.InformationUnitListItem;
+import org.remus.infomngmnt.RuleValue;
+import org.remus.infomngmnt.core.model.InformationUtil;
 import org.remus.infomngmnt.image.ImagePlugin;
+import org.remus.infomngmnt.ui.extension.AbstractCreationPreferencePage;
 import org.remus.infomngmnt.ui.newwizards.NewInfoObjectWizard;
 
 /**
@@ -57,6 +67,56 @@ public class NewImageWizard extends NewInfoObjectWizard {
 	@Override
 	protected String getInfoTypeId() {
 		return ImagePlugin.TYPE_ID;
+	}
+
+	public void setDefaults(final Object value, final RuleValue ruleValue) {
+		if (value instanceof ImageData) {
+			InformationUnit rawDataNode = InformationUtil.getChildByType(this.newElement, ImagePlugin.NODE_NAME_RAWDATA);
+			ImageLoader loader = new ImageLoader();
+			loader.data = new ImageData[] {(ImageData) value};
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			loader.save(baos, SWT.IMAGE_BMP);
+			rawDataNode.setBinaryValue(baos.toByteArray());
+			try {
+				baos.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			InformationUnit width = InformationUtil.getChildByType(this.newElement, ImagePlugin.NODE_NAME_WIDTH);
+			width.setLongValue(((ImageData) value).width);
+			InformationUnit height = InformationUtil.getChildByType(this.newElement, ImagePlugin.NODE_NAME_HEIGHT);
+			height.setLongValue(((ImageData) value).height);
+			int type = ((ImageData) value).type;
+			String extension = "bmp";
+			switch (type) {
+			case SWT.IMAGE_GIF:
+				extension = "gif";
+				break;
+			case SWT.IMAGE_JPEG:
+				extension = "jpg";
+				break;
+			case SWT.IMAGE_PNG:
+				extension = "png";
+				break;
+			default:
+				break;
+			}
+			InformationUnit origFilePath = InformationUtil.getChildByType(this.newElement, ImagePlugin.ORIGINAL_FILEPATH);
+			origFilePath.setStringValue("clipboard." + extension);
+			
+		}
+		InformationUnit predefinedCategory = InformationUtil.getChildByType(
+				ruleValue, AbstractCreationPreferencePage.NODENAME_PREDEFINED_CATEGORY);
+		InformationUnit predefinedName = InformationUtil.getChildByType(
+				ruleValue, AbstractCreationPreferencePage.NODENAME_PREDEFINED_CATEGORY);
+		if (predefinedCategory != null) {
+			this.page1.setCategoryString(predefinedCategory.getStringValue());
+		}
+		if (predefinedName != null) {
+			this.newElement.setLabel(predefinedName.getStringValue());
+		}
+		
 	}
 
 }

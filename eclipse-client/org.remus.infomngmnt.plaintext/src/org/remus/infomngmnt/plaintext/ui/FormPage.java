0c8b2effa164035e4deda650914cadbe356b15e3
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-03-11 22:14:26 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/ui/FormPage.java
+++ eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/ui/FormPage.java
@@ -9,6 +9,7 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
+
 import org.remus.infomngmnt.InfomngmntPackage;
 import org.remus.infomngmnt.ui.extension.AbstractInformationFormPage;
 
@@ -16,36 +17,28 @@ public class FormPage extends AbstractInformationFormPage {
 
 	private Text styledText;
 
-
 	@Override
-	protected void createFormContent(IManagedForm managedForm) {
+	protected void createFormContent(final IManagedForm managedForm) {
 		FormToolkit toolkit = managedForm.getToolkit();
 		ScrolledForm form = managedForm.getForm();
 		Composite body = form.getBody();
 		body.setLayout(new GridLayout());
 		toolkit.paintBordersFor(body);
 
-		this.styledText = toolkit.createText(body,"",SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL | SWT.H_SCROLL); //$NON-NLS-1$
-		this.styledText.setLayoutData(new GridData(SWT.FILL,SWT.FILL,true,true));
-		addDirtyOnTextModifyListener(this.styledText, InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
+		this.styledText = toolkit.createText(body,
+				"", SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL | SWT.H_SCROLL); //$NON-NLS-1$
+		this.styledText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		addDirtyOnTextModifyListener(this.styledText,
+				InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
 		toolkit.adapt(this.styledText, true, true);
-
 
 	}
 
 	@Override
-	public void doSave(IProgressMonitor monitor) {
-		//		getModelObject().setStringValue(this.styledText.getText());
+	public void doSave(final IProgressMonitor monitor) {
+		// getModelObject().setStringValue(this.styledText.getText());
 		setDirty(false);
 		super.doSave(monitor);
 	}
-
-	@Override
-	protected String getString() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
 
 }

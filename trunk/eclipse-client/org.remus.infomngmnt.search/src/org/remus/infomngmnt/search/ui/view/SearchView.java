bfbf3923827b0f7e6c2093b470c4f1dccb54d324
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-03-14 16:55:53 +0000

    minor changes

--- trunk/eclipse-client/org.remus.infomngmnt.search/src/org/remus/infomngmnt/search/ui/view/SearchView.java
+++ trunk/eclipse-client/org.remus.infomngmnt.search/src/org/remus/infomngmnt/search/ui/view/SearchView.java
@@ -28,9 +28,11 @@ import org.eclipse.jface.databinding.swt.ISWTObservableValue;
 import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.jface.databinding.viewers.IViewerObservableSet;
 import org.eclipse.jface.databinding.viewers.ViewersObservables;
-import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.layout.TableColumnLayout;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.nebula.widgets.calendarcombo.CalendarCombo;
 import org.eclipse.nebula.widgets.calendarcombo.DefaultSettings;
 import org.eclipse.nebula.widgets.calendarcombo.ISettings;
@@ -45,6 +47,7 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.PartInitException;
@@ -76,7 +79,8 @@ public class SearchView extends AbstractScrolledTitledView {
 	private ComposedAdapterFactory adapterFactory;
 	private AdapterFactoryEditingDomain editingDomain;
 	public static final String ID = "org.remus.infomngmnt.search.ui.view.SearchView"; //$NON-NLS-1$
-	private static final int SECTION_STYLE = ExpandableComposite.TITLE_BAR | ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED;
+	private static final int SECTION_STYLE = ExpandableComposite.TITLE_BAR
+			| ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED;
 	private CalendarCombo fromDate;
 	private CalendarCombo toDate;
 	private final ISettings calendarSettings = new DefaultSettings();
@@ -87,8 +91,6 @@ public class SearchView extends AbstractScrolledTitledView {
 	private CheckboxTableViewer viewer;
 	private HashSet<IInfoType> checkedElements;
 
-
-
 	/**
 	 * Create contents of the view part
 	 * 
@@ -96,42 +98,53 @@ public class SearchView extends AbstractScrolledTitledView {
 	 */
 	@Override
 	public void createViewContents(final Composite parent) {
-		Composite container = this.toolkit.createComposite(parent, SWT.NONE);
-		container.setLayout(new GridLayout());
-		this.toolkit.paintBordersFor(container);
 
-		this.toolkit.createLabel(container, "Search string", SWT.NONE);
+		this.toolkit.createLabel(parent, "Search string", SWT.NONE);
 
-		this.combo = new Combo(container, SWT.NONE);
+		this.combo = new Combo(parent, SWT.NONE);
 		this.toolkit.adapt(this.combo, true, true);
 		this.combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 
-		this.toolkit.createHyperlink(container, "Learn more about search syntax...", SWT.NONE);
+		this.toolkit.createHyperlink(parent, "Learn more about search syntax...", SWT.NONE);
 
-		final Section typeSection = this.toolkit.createSection(container, SECTION_STYLE);
+		final Section typeSection = this.toolkit.createSection(parent, SECTION_STYLE);
 		typeSection.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 		typeSection.setText("Information Type");
 
 		final Composite composite = this.toolkit.createComposite(typeSection, SWT.NONE);
 		typeSection.setClient(composite);
 		final GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
+		// gridLayout.numColumns = 2;
 		composite.setLayout(gridLayout);
 		this.toolkit.paintBordersFor(composite);
+		GridData layoutData = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
+		layoutData.heightHint = 80;
+		composite.setLayoutData(layoutData);
 
-		this.table = this.toolkit.createTable(composite, SWT.CHECK | SWT.V_SCROLL | SWT.H_SCROLL);
-		GridData gd_table = new GridData(SWT.FILL, SWT.BEGINNING, true, false, 1, 2);
-		gd_table.heightHint = 50;
-		this.table.setLayoutData(gd_table);
+		Composite tableComposite = this.toolkit.createComposite(composite);
+		this.table = this.toolkit.createTable(tableComposite, SWT.CHECK);
+		TableColumn singleColumn = new TableColumn(this.table, SWT.NONE);
+		TableColumnLayout tableColumnLayout = new TableColumnLayout();
+		tableColumnLayout.setColumnData(singleColumn, new ColumnWeightData(100));
+		tableComposite.setLayout(tableColumnLayout);
+
+		GridData gd_table = new GridData(SWT.FILL, SWT.FILL, true, true);
+
+		gd_table.widthHint = 1; // https://bugs.eclipse.org/bugs/show_bug.cgi?id=215997
+
+		tableComposite.setLayoutData(gd_table);
 
 		this.viewer = new CheckboxTableViewer(this.table);
 		this.viewer.setContentProvider(UIUtil.getArrayContentProviderInstance());
 		this.viewer.setInput(InformationExtensionManager.getInstance().getTypes());
+		this.viewer.setSorter(new ViewerSorter());
 		this.viewer.setLabelProvider(new LabelProvider() {
 			@Override
 			public String getText(final Object element) {
+				System.out.println("\"" + ((IInfoType) element).getName() + "\"");
 				return ((IInfoType) element).getName();
 			}
+
 			@Override
 			public Image getImage(final Object element) {
 				return ((IInfoType) element).getImage();
@@ -142,11 +155,13 @@ public class SearchView extends AbstractScrolledTitledView {
 		button_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
 		button_1.addListener(SWT.Selection, new Listener() {
 			public void handleEvent(final Event event) {
-				SearchView.this.viewer.setCheckedElements(InformationExtensionManager.getInstance().getTypes().toArray());
+				SearchView.this.viewer.setCheckedElements(InformationExtensionManager.getInstance()
+						.getTypes().toArray());
 			}
 		});
 
-		final Button deselectAllButton = this.toolkit.createButton(composite, "Deselect All", SWT.NONE);
+		final Button deselectAllButton = this.toolkit.createButton(composite, "Deselect All",
+				SWT.NONE);
 		final GridData gd_deselectAllButton = new GridData(SWT.FILL, SWT.TOP, false, false);
 		deselectAllButton.setLayoutData(gd_deselectAllButton);
 		deselectAllButton.addListener(SWT.Selection, new Listener() {
@@ -155,7 +170,7 @@ public class SearchView extends AbstractScrolledTitledView {
 			}
 		});
 
-		final Section dateSection = this.toolkit.createSection(container, SECTION_STYLE);
+		final Section dateSection = this.toolkit.createSection(parent, SECTION_STYLE);
 		final GridData gd_informationTypeSection = new GridData(SWT.FILL, SWT.CENTER, false, false);
 		dateSection.setLayoutData(gd_informationTypeSection);
 		dateSection.setText("Dates");
@@ -171,40 +186,43 @@ public class SearchView extends AbstractScrolledTitledView {
 		GridData gd_fromLabel = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
 		fromLabel.setLayoutData(gd_fromLabel);
 
-		this.fromDate = new CalendarCombo(composite_2, SWT.READ_ONLY, this.calendarSettings, null, false);
+		this.fromDate = new CalendarCombo(composite_2, SWT.READ_ONLY, this.calendarSettings, null,
+				false);
 		this.fromDate.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 
 		final Label toLabel = this.toolkit.createLabel(composite_2, "to:", SWT.NONE);
 		final GridData gd_toLabel = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
 		toLabel.setLayoutData(gd_toLabel);
 
-		this.toDate = new CalendarCombo(composite_2, SWT.READ_ONLY, this.calendarSettings, null, false);
+		this.toDate = new CalendarCombo(composite_2, SWT.READ_ONLY, this.calendarSettings, null,
+				false);
 		this.toDate.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 
-
-		final Section scopeSection = this.toolkit.createSection(container, SECTION_STYLE);
+		final Section scopeSection = this.toolkit.createSection(parent, SECTION_STYLE);
 		scopeSection.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
 		scopeSection.setText("Scope");
 
 		final Composite composite_1 = this.toolkit.createComposite(scopeSection, SWT.NONE);
 		this.toolkit.paintBordersFor(composite_1);
-		composite_1.setLayout(new GridLayout(1,false));
+		composite_1.setLayout(new GridLayout(1, false));
 		scopeSection.setClient(composite_1);
 
 		GridData gd_scopeButtons = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
 
-
-		this.allScopeButton = this.toolkit.createButton(composite_1, SearchScope.ALL.getLiteral(), SWT.RADIO);
+		this.allScopeButton = this.toolkit.createButton(composite_1, SearchScope.ALL.getLiteral(),
+				SWT.RADIO);
 		this.allScopeButton.setLayoutData(gd_scopeButtons);
-		this.selectedInfoUnit = this.toolkit.createButton(composite_1, SearchScope.SELECTED_INFO_UNIT.getLiteral(), SWT.RADIO);
+		this.selectedInfoUnit = this.toolkit.createButton(composite_1,
+				SearchScope.SELECTED_INFO_UNIT.getLiteral(), SWT.RADIO);
 		this.selectedInfoUnit.setLayoutData(gd_scopeButtons);
-		this.openEditorScopeButton = this.toolkit.createButton(composite_1, SearchScope.OPEN_EDITORS.getLiteral(), SWT.RADIO);
+		this.openEditorScopeButton = this.toolkit.createButton(composite_1,
+				SearchScope.OPEN_EDITORS.getLiteral(), SWT.RADIO);
 		this.openEditorScopeButton.setLayoutData(gd_scopeButtons);
 
-		final Label label = this.toolkit.createSeparator(container, SWT.HORIZONTAL);
+		final Label label = this.toolkit.createSeparator(parent, SWT.HORIZONTAL);
 		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
 
-		final Button button = this.toolkit.createButton(container, "Search", SWT.NONE);
+		final Button button = this.toolkit.createButton(parent, "Search", SWT.NONE);
 		button.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
 		button.addListener(SWT.Selection, new Listener() {
 			public void handleEvent(final Event event) {
@@ -212,10 +230,8 @@ public class SearchView extends AbstractScrolledTitledView {
 			}
 		});
 
-
 		initEditingDomain();
 		initDataBinding();
-
 
 		createActions();
 		initializeToolBar();
@@ -237,30 +253,31 @@ public class SearchView extends AbstractScrolledTitledView {
 		this.ctx = new EMFDataBindingContext();
 
 		// from-date
-		IObservableValue mObsFrom = EMFEditObservables.observeValue(
-				Realm.getDefault(), this.editingDomain, this.currentSearch, SearchPackage.Literals.SEARCH__DATE_START);
+		IObservableValue mObsFrom = EMFEditObservables.observeValue(Realm.getDefault(),
+				this.editingDomain, this.currentSearch, SearchPackage.Literals.SEARCH__DATE_START);
 		ISWTObservableValue uObsFrom = SWTObservables.observeText(this.fromDate.getCombo());
-		this.ctx.bindValue(uObsFrom, mObsFrom,
-				new CalendarComboTargetToModel(this.fromDate, this.calendarSettings),
-				new CalendarComboModelToTarget(this.fromDate,this.calendarSettings));
+		this.ctx.bindValue(uObsFrom, mObsFrom, new CalendarComboTargetToModel(this.fromDate,
+				this.calendarSettings), new CalendarComboModelToTarget(this.fromDate,
+				this.calendarSettings));
 
 		// to-date
-		IObservableValue mObsTo = EMFEditObservables.observeValue(
-				Realm.getDefault(), this.editingDomain, this.currentSearch, SearchPackage.Literals.SEARCH__END_DATE);
+		IObservableValue mObsTo = EMFEditObservables.observeValue(Realm.getDefault(),
+				this.editingDomain, this.currentSearch, SearchPackage.Literals.SEARCH__END_DATE);
 		ISWTObservableValue uObsTo = SWTObservables.observeText(this.toDate.getCombo());
-		this.ctx.bindValue(uObsTo, mObsTo,
-				new CalendarComboTargetToModel(this.toDate, this.calendarSettings),
-				new CalendarComboModelToTarget(this.toDate,this.calendarSettings));
+		this.ctx.bindValue(uObsTo, mObsTo, new CalendarComboTargetToModel(this.toDate,
+				this.calendarSettings), new CalendarComboModelToTarget(this.toDate,
+				this.calendarSettings));
 
 		// scope
-		IObservableValue mObsAllScope = EMFEditObservables.observeValue(
-				Realm.getDefault(), this.editingDomain, this.currentSearch, SearchPackage.Literals.SEARCH__SCOPE);
+		IObservableValue mObsAllScope = EMFEditObservables.observeValue(Realm.getDefault(),
+				this.editingDomain, this.currentSearch, SearchPackage.Literals.SEARCH__SCOPE);
 
-		Map<Button,Enumerator> buttonMapping = new HashMap<Button, Enumerator>();
+		Map<Button, Enumerator> buttonMapping = new HashMap<Button, Enumerator>();
 		buttonMapping.put(this.allScopeButton, SearchScope.ALL);
 		buttonMapping.put(this.selectedInfoUnit, SearchScope.SELECTED_INFO_UNIT);
 		buttonMapping.put(this.openEditorScopeButton, SearchScope.OPEN_EDITORS);
-		RadioButtonGroupEnumBinding radioButtonCtx = new RadioButtonGroupEnumBinding(buttonMapping,this.ctx);
+		RadioButtonGroupEnumBinding radioButtonCtx = new RadioButtonGroupEnumBinding(buttonMapping,
+				this.ctx);
 		radioButtonCtx.setDefault(this.currentSearch.getScope());
 		radioButtonCtx.bind(mObsAllScope);
 
@@ -268,13 +285,16 @@ public class SearchView extends AbstractScrolledTitledView {
 		EList<String> infoType = this.currentSearch.getInfoType();
 		final IObservableList list = new WritableList();
 		for (String string : infoType) {
-			// at first we have to create a list for the binding to the emf list.
+			// at first we have to create a list for the binding to the emf
+			// list.
 			list.add(InformationExtensionManager.getInstance().getInfoTypeByType(string));
 		}
-		// we have to set the initial selection manually because we are syncing just
+		// we have to set the initial selection manually because we are syncing
+		// just
 		// in one direction.
 		this.viewer.setCheckedElements(list.toArray());
-		IViewerObservableSet elements = ViewersObservables.observeCheckedElements(this.viewer, new HashSet());
+		IViewerObservableSet elements = ViewersObservables.observeCheckedElements(this.viewer,
+				new HashSet());
 		// we convert all changes from the set to the list
 		elements.addSetChangeListener(new ISetChangeListener() {
 			public void handleSetChange(final SetChangeEvent event) {
@@ -283,11 +303,13 @@ public class SearchView extends AbstractScrolledTitledView {
 				list.addAll(diff.getAdditions());
 			}
 		});
-		IObservableList observeList2 = EMFObservables.observeList(this.currentSearch, SearchPackage.Literals.SEARCH__INFO_TYPE);
-		this.ctx.bindList(observeList2, list,  new UpdateListStrategy() {
+		IObservableList observeList2 = EMFObservables.observeList(this.currentSearch,
+				SearchPackage.Literals.SEARCH__INFO_TYPE);
+		this.ctx.bindList(observeList2, list, new UpdateListStrategy() {
 			@Override
 			public Object convert(final Object element) {
-				return InformationExtensionManager.getInstance().getInfoTypeByType((String) element);
+				return InformationExtensionManager.getInstance()
+						.getInfoTypeByType((String) element);
 			}
 		}, new UpdateListStrategy() {
 			@Override
@@ -296,9 +318,10 @@ public class SearchView extends AbstractScrolledTitledView {
 			}
 		});
 
-
 		// latest search entries
-		IObservableList observeList = EMFEditObservables.observeList(Realm.getDefault(), this.editingDomain , this.latestSearchStrings, SearchPackage.Literals.LATEST_SEARCH_STRINGS__STRINGS);
+		IObservableList observeList = EMFEditObservables.observeList(Realm.getDefault(),
+				this.editingDomain, this.latestSearchStrings,
+				SearchPackage.Literals.LATEST_SEARCH_STRINGS__STRINGS);
 		IObservableList observeItems = SWTObservables.observeItems(this.combo);
 		this.ctx.bindList(observeItems, observeList, new UpdateListStrategy() {
 			@Override
@@ -309,10 +332,11 @@ public class SearchView extends AbstractScrolledTitledView {
 		}, null);
 
 		// search-text
-		IObservableValue mObsText = EMFEditObservables.observeValue(
-				Realm.getDefault(), this.editingDomain, this.currentSearch, SearchPackage.Literals.SEARCH__SEARCH_STRING);
+		IObservableValue mObsText = EMFEditObservables.observeValue(Realm.getDefault(),
+				this.editingDomain, this.currentSearch,
+				SearchPackage.Literals.SEARCH__SEARCH_STRING);
 		ISWTObservableValue uObsText = SWTObservables.observeText(this.combo);
-		this.ctx.bindValue(uObsText, mObsText,null,null);
+		this.ctx.bindValue(uObsText, mObsText, null, null);
 
 	}
 
@@ -322,11 +346,13 @@ public class SearchView extends AbstractScrolledTitledView {
 	 * which are displayed in this viewpart.
 	 */
 	private void initEditingDomain() {
-		this.adapterFactory = new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
+		this.adapterFactory = new ComposedAdapterFactory(
+				ComposedAdapterFactory.Descriptor.Registry.INSTANCE);
 		this.adapterFactory.addAdapterFactory(new ResourceItemProviderAdapterFactory());
 		this.adapterFactory.addAdapterFactory(new SearchItemProviderAdapterFactory());
 		this.adapterFactory.addAdapterFactory(new ReflectiveItemProviderAdapterFactory());
-		this.editingDomain = new AdapterFactoryEditingDomain(this.adapterFactory, new BasicCommandStack());
+		this.editingDomain = new AdapterFactoryEditingDomain(this.adapterFactory,
+				new BasicCommandStack());
 	}
 
 	@Override
@@ -375,7 +401,5 @@ public class SearchView extends AbstractScrolledTitledView {
 		}
 		return this.savedSearchHandler;
 	}
-
-
 
 }

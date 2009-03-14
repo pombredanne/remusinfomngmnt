b8a9ed364448abb3ba261d1c6d87bb9d98174197
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-03-14 14:57:23 +0000

    minor changes

--- trunk/eclipse-client/org.remus.infomngmnt.core/src/org/remus/infomngmnt/core/remote/IRepository.java
+++ trunk/eclipse-client/org.remus.infomngmnt.core/src/org/remus/infomngmnt/core/remote/IRepository.java
@@ -24,60 +24,76 @@ import org.remus.infomngmnt.RemoteObject;
 import org.remus.infomngmnt.SynchronizableObject;
 
 /**
+ * This interface defines a repository, which can synchronize between the local
+ * and the remote data, based on an entry point. The idea is to make integration
+ * of different connectors as easy as possible.
+ * 
  * @author Tom Seidel <tom.seidel@remus-software.org>
  */
 public interface IRepository {
-	
+
 	/**
-	 * Returns an image (typically 16x16) which represents
-	 * a specific repository through the whole application.
+	 * Returns an image (typically 16x16) which represents a specific repository
+	 * through the whole application.
+	 * 
 	 * @return the repository specific image.
 	 * @since 1.0
 	 */
 	Image getImage();
-	
+
 	/**
+	 * Returns the label of the repository.
+	 * 
 	 * @return
 	 */
 	String getLabel();
-	
+
 	String getId();
 
 	void login(ILoginCallBack callback, IProgressMonitor monitor);
 
 	String getRepositoryUrl();
-	
-	RemoteObject[] getChildren(IProgressMonitor monitor, RemoteContainer container, boolean showOnlyContainers);
-	
+
+	RemoteObject[] getChildren(IProgressMonitor monitor, RemoteContainer container,
+			boolean showOnlyContainers);
+
 	IStatus validate();
-	
+
 	void setCredentialProvider(ICredentialProvider provider);
-	
+
 	ICredentialProvider getCredentialProvider();
-	
+
 	void reset();
-	
+
+	/**
+	 * Returns the scheduling rule for calling
+	 * {@link #getChildren(IProgressMonitor, RemoteContainer, boolean)} or other
+	 * methods which access directy to the remote repository.
+	 * 
+	 * @return the rule the appropriate job is scheduled.
+	 */
 	ISchedulingRule getRule();
 
 	String getLocalRepositoryId();
 
 	void setLocalRepositoryId(final String localRepositoryId);
-	
+
 	String getTypeIdByObject(RemoteObject remoteObject);
-	
+
 	InformationUnit getPrefetchedInformationUnit(RemoteObject remoteObject);
 
 	InformationUnit getFullObject(InformationUnitListItem informationUnitListItem);
 
 	/**
 	 * Commits a {@link SynchronizableObject}.
+	 * 
 	 * @param item2commit
 	 * @param monitor
 	 */
 	String commit(SynchronizableObject item2commit, IProgressMonitor monitor);
-	
+
 	RemoteObject addToRepository(SynchronizableObject item, IProgressMonitor monitor);
-	
+
 	RemoteObject getRemoteObjectBySynchronizableObject(final SynchronizableObject object);
 
 }

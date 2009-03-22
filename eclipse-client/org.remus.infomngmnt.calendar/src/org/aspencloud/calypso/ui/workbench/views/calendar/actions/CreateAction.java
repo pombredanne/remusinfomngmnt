6cb4eebb760f5343559c259177538428d3141ca6
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-03-22 23:51:48 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.calendar/src/org/aspencloud/calypso/ui/workbench/views/calendar/actions/CreateAction.java
+++ eclipse-client/org.remus.infomngmnt.calendar/src/org/aspencloud/calypso/ui/workbench/views/calendar/actions/CreateAction.java
@@ -1,13 +1,13 @@
 /****************************************************************************
-* Copyright (c) 2005-2006 Jeremy Dowdall
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Jeremy Dowdall <aspencloud@users.sourceforge.net> - initial API and implementation
-*****************************************************************************/
+ * Copyright (c) 2005-2006 Jeremy Dowdall
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jeremy Dowdall <aspencloud@users.sourceforge.net> - initial API and implementation
+ *****************************************************************************/
 
 package org.aspencloud.calypso.ui.workbench.views.calendar.actions;
 
@@ -19,7 +19,6 @@ import org.eclipse.gef.Tool;
 import org.eclipse.gef.tools.AbstractTool;
 import org.eclipse.gef.tools.CreationTool;
 import org.eclipse.jface.action.Action;
-import org.eclipse.swt.SWT;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 
@@ -27,27 +26,33 @@ public class CreateAction extends Action {
 	public static final String ID = "org.aspencloud.calypso.ui.views.Calendar.actions.ShowGridAction";
 
 	private final GraphicalViewer[] viewers;
-	
+
 	public CreateAction(final GraphicalViewer[] viewers) {
-		super("Create", SWT.CHECK);
+		super("Create");
 		this.viewers = viewers;
 		setToolTipText("Toggle Creation Tool");
-		setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJ_ELEMENT));
+		setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(
+				ISharedImages.IMG_OBJ_ELEMENT));
 		setId(ID);
-		setChecked(false);
-		run();
+
 	}
 
 	@Override
 	public void run() {
 		Tool tool = null;
-		if(isChecked()) {
-			tool = new CreationTool(new ModelFactory(ModelFactory.TASK_TEMPLATE));
-			tool.setProperties(Collections.singletonMap(AbstractTool.PROPERTY_UNLOAD_WHEN_FINISHED, Boolean.FALSE));
+		if (true) {
+			tool = new CreationTool(new ModelFactory(ModelFactory.TASK_TEMPLATE)) {
+				@Override
+				protected void performCreation(final int button) {
+					super.performCreation(button);
+				}
+			};
+			tool.setProperties(Collections.singletonMap(AbstractTool.PROPERTY_UNLOAD_WHEN_FINISHED,
+					Boolean.TRUE));
 		}
 
-		for(int i = 0; i < this.viewers.length; i++) {
-			if(tool != null) {
+		for (int i = 0; i < this.viewers.length; i++) {
+			if (tool != null) {
 				this.viewers[i].getEditDomain().setActiveTool(tool);
 			} else {
 				this.viewers[i].getEditDomain().loadDefaultTool();
--- eclipse-client/org.remus.infomngmnt.calendar/src/org/aspencloud/calypso/ui/workbench/views/calendar/actions/ZoomInAction.java
+++ eclipse-client/org.remus.infomngmnt.calendar/src/org/aspencloud/calypso/ui/workbench/views/calendar/actions/ZoomInAction.java
@@ -1,35 +1,36 @@
 /****************************************************************************
-* Copyright (c) 2005-2006 Jeremy Dowdall
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Jeremy Dowdall <aspencloud@users.sourceforge.net> - initial API and implementation
-*****************************************************************************/
+ * Copyright (c) 2005-2006 Jeremy Dowdall
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jeremy Dowdall <aspencloud@users.sourceforge.net> - initial API and implementation
+ *****************************************************************************/
 
 package org.aspencloud.calypso.ui.workbench.views.calendar.actions;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import org.remus.infomngmnt.ccalendar.CCalendar;
 
 public class ZoomInAction extends Action {
 	public static final String ID = "org.aspencloud.calypso.ui.views.Calendar.stuff.actions.ZoomInAction";
 
-	private final CCalendar calendar;
-	
-	public ZoomInAction(final CCalendar calendar) {
+	private CCalendar calendar;
+
+	public ZoomInAction() {
 		super("Zoom In");
-		this.calendar = calendar;
 		setToolTipText("Zoom In");
-		setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OBJ_ELEMENT));
+		setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(CCalendar.PLUGIN_ID,
+				"icons/iconexperience/16/zoom_in.png"));
 		setId(ID);
-//		setChecked(true);
-//		run();
+	}
+
+	public void setCalendar(final CCalendar calendar) {
+		this.calendar = calendar;
 	}
 
 	@Override

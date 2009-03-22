eccf06e616c863ff2074dc5ddd908ec8e6e330e0
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-03-22 23:53:32 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.efs/src/org/remus/infomngmnt/efs/EFSActivator.java
+++ eclipse-client/org.remus.infomngmnt.efs/src/org/remus/infomngmnt/efs/EFSActivator.java
@@ -1,8 +1,17 @@
 package org.remus.infomngmnt.efs;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+
+import org.remus.infomngmnt.efs.extension.AbstractSecurityProvider;
+import org.remus.infomngmnt.efs.extension.ISecurityProviderExtension;
+import org.remus.infomngmnt.efs.internal.model.SecurityWrapper;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -64,6 +73,39 @@ public class EFSActivator extends AbstractUIPlugin {
 			return (T) service;
 		}
 		return null;
+	}
+
+	public List<SecurityWrapper> getSecurityProvideProjects() {
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		List<SecurityWrapper> returnValue = new ArrayList<SecurityWrapper>();
+		for (IProject iProject : projects) {
+			AbstractSecurityProvider providerByFileSystem = getService(
+					ISecurityProviderExtension.class).getProviderByFileSystem(
+					iProject.getLocationURI().getScheme());
+			if (providerByFileSystem != null && !providerByFileSystem.isInitialized()) {
+				SecurityWrapper securityProvider = getProviderByExtension(returnValue,
+						providerByFileSystem);
+				if (securityProvider == null) {
+					securityProvider = new SecurityWrapper(providerByFileSystem.getName(),
+							providerByFileSystem);
+					returnValue.add(securityProvider);
+				}
+				securityProvider.addProject(iProject);
+				System.out.println(providerByFileSystem.getName());
+			}
+		}
+		return returnValue;
+	}
+
+	private static SecurityWrapper getProviderByExtension(final List<SecurityWrapper> list,
+			final AbstractSecurityProvider provider) {
+		for (SecurityWrapper securityProvider : list) {
+			if (securityProvider.getProvider() == provider) {
+				return securityProvider;
+			}
+		}
+		return null;
+
 	}
 
 }
--- eclipse-client/org.remus.infomngmnt.efs/src/org/remus/infomngmnt/efs/ui/InitializeSecurityProviderDialog.java
+++ eclipse-client/org.remus.infomngmnt.efs/src/org/remus/infomngmnt/efs/ui/InitializeSecurityProviderDialog.java
@@ -1,35 +1,50 @@
 package org.remus.infomngmnt.efs.ui;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
-import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.ui.forms.widgets.TableWrapData;
-import org.eclipse.ui.forms.widgets.TableWrapLayout;
+
+import org.remus.infomngmnt.efs.internal.model.SecurityAffectedProject;
+import org.remus.infomngmnt.efs.internal.model.SecurityWrapper;
 
 public class InitializeSecurityProviderDialog extends TitleAreaDialog {
 
 	private Table table_1;
 	private Table table;
+	private final List<SecurityWrapper> model;
 
 	/**
 	 * Create the dialog
 	 * 
 	 * @param parentShell
 	 */
-	public InitializeSecurityProviderDialog(final Shell parentShell) {
+	public InitializeSecurityProviderDialog(final Shell parentShell,
+			final List<SecurityWrapper> model) {
 		super(parentShell);
+		this.model = model;
+	}
+
+	@Override
+	protected void configureShell(final Shell newShell) {
+		super.configureShell(newShell);
 	}
 
 	/**
@@ -40,65 +55,27 @@ public class InitializeSecurityProviderDialog extends TitleAreaDialog {
 	@Override
 	protected Control createDialogArea(final Composite parent) {
 		Composite area = (Composite) super.createDialogArea(parent);
-		Composite container = new Composite(area, SWT.NONE);
-		final TableWrapLayout tableWrapLayout = new TableWrapLayout();
-		container.setLayout(tableWrapLayout);
-		container.setLayoutData(new GridData(GridData.FILL_BOTH));
+		final Composite container = new Composite(area, SWT.NONE);
+		container.setLayout(new GridLayout());
+		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
 		final Label remusHasFoundLabel = new Label(container, SWT.WRAP);
-		final TableWrapData twd_remusHasFoundLabel = new TableWrapData(TableWrapData.FILL_GRAB,
-				TableWrapData.FILL);
-		twd_remusHasFoundLabel.grabHorizontal = true;
-		remusHasFoundLabel.setLayoutData(twd_remusHasFoundLabel);
+		remusHasFoundLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.FILL, false, false));
 		remusHasFoundLabel
-				.setText("RIM has found projects which are encrypted. Before accessing to the content of this projects you have to initialize the appropiated security addins which can decrypt the content.");
+				.setText("RIM has found projects which are encrypted and whose security provider needs input. Before accessing to the content of this projects you have to initialize the appropiated security addins which can decrypt the content.");
+		getShell().addListener(SWT.RESIZE, new Listener() {
+			public void handleEvent(final Event event) {
+				GridData layoutData = (GridData) remusHasFoundLabel.getLayoutData();
+				layoutData.widthHint = getShell().getBounds().width - 20;
+				container.layout();
+			}
+		});
 
-		final Composite composite_2 = new Composite(container, SWT.NONE);
-		composite_2.setLayoutData(new TableWrapData(TableWrapData.FILL, TableWrapData.FILL_GRAB));
-		composite_2.setLayout(new FillLayout());
+		EncryptedProjectViewer viewer = new EncryptedProjectViewer(container, this.model);
+		viewer.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 
-		final SashForm sashForm = new SashForm(composite_2, SWT.NONE);
-
-		final Composite composite = new Composite(sashForm, SWT.NONE);
-		composite.setLayout(new GridLayout());
-
-		final Label providersNeedsInitializationLabel = new Label(composite, SWT.NONE);
-		providersNeedsInitializationLabel.setText("Providers needs initialization");
-
-		final TableViewer tableViewer = new TableViewer(composite, SWT.BORDER);
-		this.table = tableViewer.getTable();
-		final GridData gd_table = new GridData(SWT.FILL, SWT.FILL, true, false);
-		gd_table.heightHint = 100;
-		this.table.setLayoutData(gd_table);
-
-		final Composite composite_1 = new Composite(sashForm, SWT.NONE);
-		final GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 3;
-		composite_1.setLayout(gridLayout);
-
-		final Label affectedProjectsLabel = new Label(composite_1, SWT.NONE);
-		affectedProjectsLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
-		affectedProjectsLabel.setText("Affected projects");
-
-		final TableViewer tableViewer_1 = new TableViewer(composite_1, SWT.BORDER);
-		this.table_1 = tableViewer_1.getTable();
-		final GridData gd_table_1 = new GridData(SWT.FILL, SWT.FILL, true, false, 3, 1);
-		gd_table_1.heightHint = 60;
-		this.table_1.setLayoutData(gd_table_1);
-
-		final Label label_1 = new Label(composite_1, SWT.NONE);
-		label_1.setText("Label");
-
-		final Label label = new Label(composite_1, SWT.NONE);
-		label.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false));
-		label.setText("Label");
-
-		final Button initializeButton = new Button(composite_1, SWT.NONE);
-		final GridData gd_initializeButton = new GridData(SWT.RIGHT, SWT.CENTER, false, false);
-		initializeButton.setLayoutData(gd_initializeButton);
-		initializeButton.setText("Initialize");
-		sashForm.setWeights(new int[] { 1, 1 });
-		//
+		final Label label = new Label(area, SWT.SEPARATOR | SWT.HORIZONTAL);
+		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
 		return area;
 	}
 
@@ -113,12 +90,48 @@ public class InitializeSecurityProviderDialog extends TitleAreaDialog {
 		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
 	}
 
+	@Override
+	protected void okPressed() {
+		ProgressMonitorDialog progressMonitorDialog = new ProgressMonitorDialog(getShell());
+		try {
+			progressMonitorDialog.run(true, false, new IRunnableWithProgress() {
+				public void run(final IProgressMonitor monitor) throws InvocationTargetException,
+						InterruptedException {
+					for (SecurityWrapper provider : InitializeSecurityProviderDialog.this.model) {
+						if (provider.getProvider().isInitialized()) {
+							List<SecurityAffectedProject> affectedProject = provider
+									.getAffectedProject();
+							for (SecurityAffectedProject securityProvderAffectedProject : affectedProject) {
+								IProject project = securityProvderAffectedProject.getProject();
+								if (!project.isOpen()) {
+									try {
+										project.open(monitor);
+									} catch (CoreException e) {
+										// hm, ok. We've done our best.
+									}
+								}
+							}
+						}
+					}
+				}
+			});
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		super.okPressed();
+	}
+
 	/**
 	 * Return the initial size of the dialog
 	 */
 	@Override
 	protected Point getInitialSize() {
-		return new Point(500, 375);
+		return new Point(500, 500);
 	}
 
 }

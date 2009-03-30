1ccda16b3804180c237a13c28cd3840949e54213
jhartwig <jhartwig@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-03-30 15:15:53 +0000

    add body for sections

--- trunk/eclipse-client/org.remus.infomngmnt.contact/src/org/remus/infomngmnt/contact/ui/EditContactPage.java
+++ trunk/eclipse-client/org.remus.infomngmnt.contact/src/org/remus/infomngmnt/contact/ui/EditContactPage.java
@@ -1,10 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Jan Hartwig, FEB Radebeul
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Jan Hartwig - initial API and implementation
+ *******************************************************************************/
 package org.remus.infomngmnt.contact.ui;
-
+/**
+ * @author Jan Hartwig <jhartwig@feb-radebeul.de>
+ * 
+ */
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.Section;
 import org.remus.infomngmnt.ui.extension.AbstractInformationFormPage;
 
 public class EditContactPage extends AbstractInformationFormPage {
@@ -22,6 +40,49 @@ public class EditContactPage extends AbstractInformationFormPage {
 		body.setLayout(new GridLayout());
 		toolkit.paintBordersFor(body);
 
+		doCreateGeneralSection(body, toolkit);
+		doCreateDetailsSection(body, toolkit);
+		doCreateEncryptionSection(body, toolkit);
+		doCreateMiscellaneousSection(body, toolkit);
+		doCreateUserDefinedSection(body, toolkit);
 		doCreateSemanticSection(body, toolkit);
+	}
+
+	private void doCreateGeneralSection(Composite body, FormToolkit toolkit) {
+		
+		final Section section_1 = toolkit.createSection(body, ExpandableComposite.TITLE_BAR
+				| ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED);
+		section_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		section_1.setText("General");
+		
+	}
+	private void doCreateDetailsSection(Composite body, FormToolkit toolkit) {
+		
+		final Section section_1 = toolkit.createSection(body, ExpandableComposite.TITLE_BAR
+				| ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED);
+		section_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		section_1.setText("Details");
+		
+	}
+	private void doCreateUserDefinedSection(Composite body, FormToolkit toolkit) {
+		
+		final Section section_1 = toolkit.createSection(body, ExpandableComposite.TITLE_BAR
+				| ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED);
+		section_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		section_1.setText("User Defined");	
+	}
+	private void doCreateEncryptionSection(Composite body, FormToolkit toolkit) {
+		
+		final Section section_1 = toolkit.createSection(body, ExpandableComposite.TITLE_BAR
+				| ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED);
+		section_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		section_1.setText("Encryption");	
+	}
+private void doCreateMiscellaneousSection(Composite body, FormToolkit toolkit) {
+		
+		final Section section_1 = toolkit.createSection(body, ExpandableComposite.TITLE_BAR
+				| ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED);
+		section_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		section_1.setText("Miscellaneous");	
 	}
 }

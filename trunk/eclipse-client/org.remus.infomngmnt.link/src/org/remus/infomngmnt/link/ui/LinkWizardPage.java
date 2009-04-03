d2ee2a200a3272e3128e40bb267e25127bc40d87
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-04-03 21:08:27 +0000

    wizard polishing

--- trunk/eclipse-client/org.remus.infomngmnt.link/src/org/remus/infomngmnt/link/ui/LinkWizardPage.java
+++ trunk/eclipse-client/org.remus.infomngmnt.link/src/org/remus/infomngmnt/link/ui/LinkWizardPage.java
@@ -13,51 +13,56 @@ import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Listener;
 
 import org.remus.infomngmnt.InformationUnit;
+import org.remus.infomngmnt.common.ui.image.ResourceManager;
 import org.remus.infomngmnt.link.LinkActivator;
 import org.remus.infomngmnt.link.preferences.LinkPreferenceInitializer;
 import org.remus.infomngmnt.link.webshot.WebshotUtil;
 
 public class LinkWizardPage extends WizardPage {
 
-
-
 	private final InformationUnit newElement;
 
-	public LinkWizardPage(InformationUnit newElement) {
+	public LinkWizardPage(final InformationUnit newElement) {
 		super("wizardPage");
 		this.newElement = newElement;
-		setTitle("Wizard Page title");
-		setDescription("Wizard Page description");
 
 	}
 
 	/**
 	 * Create contents of the wizard
+	 * 
 	 * @param parent
 	 */
-	public void createControl(Composite parent) {
+	public void createControl(final Composite parent) {
 		Composite container = new Composite(parent, SWT.NULL);
 		container.setLayout(new GridLayout());
 		final IPreferenceStore store = LinkActivator.getDefault().getPreferenceStore();
 		//
+
+		setTitle("New Link");
+		setMessage("This wizard enables you to create a new link from a url.");
+		setImageDescriptor(ResourceManager.getPluginImageDescriptor(LinkActivator.getDefault(),
+				"icons/iconexperience/link_wizard_title.png"));
+
 		setControl(container);
 
 		final Button makeContentSearchableButton = new Button(container, SWT.CHECK);
 		makeContentSearchableButton.setText("Make Content searchable");
 		makeContentSearchableButton.addListener(SWT.Selection, new Listener() {
 
-			public void handleEvent(Event event) {
-				store.setValue(LinkPreferenceInitializer.INDEX_DOCUMENT,
-						((Button) event.widget).getSelection());
+			public void handleEvent(final Event event) {
+				store.setValue(LinkPreferenceInitializer.INDEX_DOCUMENT, ((Button) event.widget)
+						.getSelection());
 
 			}
 
 		});
-		makeContentSearchableButton.setSelection(
-				store.getBoolean(LinkPreferenceInitializer.INDEX_DOCUMENT));
+		makeContentSearchableButton.setSelection(store
+				.getBoolean(LinkPreferenceInitializer.INDEX_DOCUMENT));
 
 		final Label theContentWillLabel = new Label(container, SWT.NONE);
-		theContentWillLabel.setText("The content will be indexed and is avaialable for the local search.");
+		theContentWillLabel
+				.setText("The content will be indexed and is avaialable for the local search.");
 
 		final Label label = new Label(container, SWT.SEPARATOR | SWT.HORIZONTAL);
 		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
@@ -67,21 +72,19 @@ public class LinkWizardPage extends WizardPage {
 		makeScreenhotOfButton.setText("Make Screenshot of target");
 		makeScreenhotOfButton.addListener(SWT.Selection, new Listener() {
 
-			public void handleEvent(Event event) {
-				store.setValue(LinkPreferenceInitializer.MAKE_SCREENSHOT,
-						((Button) event.widget).getSelection());
+			public void handleEvent(final Event event) {
+				store.setValue(LinkPreferenceInitializer.MAKE_SCREENSHOT, ((Button) event.widget)
+						.getSelection());
 
 			}
 
 		});
-		makeScreenhotOfButton.setSelection(
-				store.getBoolean(LinkPreferenceInitializer.MAKE_SCREENSHOT));
+		makeScreenhotOfButton.setSelection(store
+				.getBoolean(LinkPreferenceInitializer.MAKE_SCREENSHOT));
 		makeScreenhotOfButton.setEnabled(WebshotUtil.isWebShotToolingEnabled());
 		final Link eclipseorgLink = new Link(container, SWT.NONE);
 		eclipseorgLink.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
 		eclipseorgLink.setText("<a>Why is the screenshot feature disabled?</a>");
 	}
-
-
 
 }

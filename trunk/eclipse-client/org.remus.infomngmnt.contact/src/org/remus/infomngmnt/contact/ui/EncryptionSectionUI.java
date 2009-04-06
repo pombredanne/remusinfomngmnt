cfaefa4e6ab43b314af3e59e2ebbf2016db0808a
jhartwig <jhartwig@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-04-06 08:57:08 +0000

    - add edit page body for section encryption

--- trunk/eclipse-client/org.remus.infomngmnt.contact/src/org/remus/infomngmnt/contact/ui/EncryptionSectionUI.java
+++ trunk/eclipse-client/org.remus.infomngmnt.contact/src/org/remus/infomngmnt/contact/ui/EncryptionSectionUI.java
@@ -17,7 +17,12 @@ package org.remus.infomngmnt.contact.ui;
   */
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.ExpandableComposite;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
@@ -29,6 +34,40 @@ public class EncryptionSectionUI {
 				| ExpandableComposite.TWISTIE);
 		section_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 		section_1.setText("Encryption");
+		
+		final Composite compositeEncryption = toolkit.createComposite(section_1, SWT.BORDER);
+		final GridLayout gridLayoutEncryption = new GridLayout();
+		gridLayoutEncryption.numColumns = 2;
+		compositeEncryption.setLayout(gridLayoutEncryption);
+		toolkit.paintBordersFor(compositeEncryption);
+		section_1.setClient(compositeEncryption);
+
+		createGroupPreferredKeys(compositeEncryption, toolkit);
+	}
+
+	private void createGroupPreferredKeys(Composite compositeEncryption,
+			FormToolkit toolkit) {
+		final Group group_Keys = new Group(compositeEncryption, SWT.NONE);
+	    final GridData gd_Keys= new GridData();
+	    gd_Keys.grabExcessVerticalSpace = true;
+	    gd_Keys.verticalAlignment = GridData.FILL;
+	    gd_Keys.grabExcessHorizontalSpace = true;
+	    gd_Keys.horizontalAlignment = GridData.FILL;
+	    group_Keys.setLayoutData(gd_Keys);
+		final GridLayout gl_GroupKeys = new GridLayout();
+		gl_GroupKeys.numColumns = 2;
+		group_Keys.setLayout(gl_GroupKeys);
+		
+		GridData gd_text = new GridData(SWT.FILL, SWT.BEGINNING, true, false);
+		gd_text.horizontalAlignment = SWT.FILL;
+		
+		toolkit.createLabel(group_Keys, "Preferred OpenPGP Encryption Key:");
+		final Text tx_OpenGpg = toolkit.createText(group_Keys, null, SWT.BORDER | SWT.FILL);
+		tx_OpenGpg.setLayoutData(gd_text);
+		toolkit.createLabel(group_Keys, "Preferred S/MIME Encryption Certificate:");
+		final Text tx_Smime = toolkit.createText(group_Keys, null, SWT.BORDER);
+		tx_Smime.setLayoutData(gd_text);
+		
 	}
 
 }

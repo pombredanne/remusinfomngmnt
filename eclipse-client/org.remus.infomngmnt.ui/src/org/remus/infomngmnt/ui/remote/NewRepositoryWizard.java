16633f95864f0730bb9f1955a88d9cdb0a8315e7
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-04-15 19:47:33 +0000

    introduced idfactory instead of universaluniqueidentifier from eclipse

--- eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/remote/NewRepositoryWizard.java
+++ eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/remote/NewRepositoryWizard.java
@@ -12,7 +12,6 @@
 
 package org.remus.infomngmnt.ui.remote;
 
-import org.eclipse.core.internal.utils.UniversalUniqueIdentifier;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 
@@ -20,6 +19,7 @@ import org.remus.infomngmnt.InfomngmntFactory;
 import org.remus.infomngmnt.RemoteRepository;
 import org.remus.infomngmnt.RepositoryCollection;
 import org.remus.infomngmnt.core.model.EditingUtil;
+import org.remus.infomngmnt.core.model.IdFactory;
 import org.remus.infomngmnt.core.services.IRepositoryService;
 import org.remus.infomngmnt.provider.InfomngmntEditPlugin;
 import org.remus.infomngmnt.ui.extension.IRepositoryUI;
@@ -28,41 +28,45 @@ import org.remus.infomngmnt.ui.extension.IRepositoryUI;
  * @author Tom Seidel <tom.seidel@remus-software.org>
  */
 public abstract class NewRepositoryWizard extends Wizard {
-	
+
 	protected RemoteRepository repository;
 	private String repositoryId;
 
 	public NewRepositoryWizard() {
 		setNeedsProgressMonitor(true);
 	}
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
 	 */
 	@Override
 	public boolean performFinish() {
-		RepositoryCollection repositories = InfomngmntEditPlugin.getPlugin().getService(IRepositoryService.class).getRepositories();
+		RepositoryCollection repositories = InfomngmntEditPlugin.getPlugin().getService(
+				IRepositoryService.class).getRepositories();
 		repositories.getRepositories().add(this.repository);
 		EditingUtil.getInstance().saveObjectToResource(repositories);
 		return true;
 	}
-	
+
 	public void init(final IStructuredSelection selection) {
 		if (!selection.isEmpty() && selection.getFirstElement() instanceof IRepositoryUI) {
 			this.repositoryId = ((IRepositoryUI) selection.getFirstElement()).getRepositoryId();
 		}
 	}
-	
+
 	@SuppressWarnings("restriction")
 	protected RemoteRepository getRepository() {
 		if (this.repository == null) {
 			this.repository = InfomngmntFactory.eINSTANCE.createRemoteRepository();
-			this.repository.setId(new UniversalUniqueIdentifier().toString());
+			this.repository.setId(IdFactory.createNewId(null));
 			this.repository.setRepositoryTypeId(this.repositoryId);
 			configureRepository(this.repository);
 		}
 		return this.repository;
 	}
-	
+
 	protected abstract void configureRepository(RemoteRepository newRemoteRepositry);
 
 }

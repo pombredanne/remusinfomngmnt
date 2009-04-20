96f10c503a1c0bae96ab7a214b187b8369241f5b
jhartwig <jhartwig@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-04-20 17:57:32 +0000

    - add phone number type values

--- trunk/eclipse-client/org.remus.infomngmnt.contact/src/org/remus/infomngmnt/contact/core/ContactCreationFactory.java
+++ trunk/eclipse-client/org.remus.infomngmnt.contact/src/org/remus/infomngmnt/contact/core/ContactCreationFactory.java
@@ -34,12 +34,54 @@ public class ContactCreationFactory extends AbstractCreationFactory {
 		InformationUnit rawDataLogo = InfomngmntFactory.eINSTANCE.createInformationUnit();
 		rawDataLogo.setType(ContactActivator.NODE_NAME_RAWDATA_LOGO);
 		InformationUnit origFilePath = InfomngmntFactory.eINSTANCE.createInformationUnit();
-		origFilePath.setType(ContactActivator.ORIGINAL_FILEPATH);
-
+		origFilePath.setType(ContactActivator.ORIGINAL_FILEPATH);		
+		InformationUnit pnHome = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnHome.setType(ContactActivator.NODE_NAME_PN_HOME);
+		InformationUnit pnWork = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnWork.setType(ContactActivator.NODE_NAME_PN_WORK);
+		InformationUnit pnMessanger = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnMessanger.setType(ContactActivator.NODE_NAME_PN_MESSANGER);
+		InformationUnit pnVoice = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnVoice.setType(ContactActivator.NODE_NAME_PN_VOICE);
+		InformationUnit pnFax = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnFax.setType(ContactActivator.NODE_NAME_PN_FAX);
+		InformationUnit pnMobile = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnMobile.setType(ContactActivator.NODE_NAME_PN_MOBILE);
+		InformationUnit pnMailbox = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnMailbox.setType(ContactActivator.NODE_NAME_PN_MAILBOX);
+		InformationUnit pnModem = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnModem.setType(ContactActivator.NODE_NAME_PN_MODEM);
+		InformationUnit pnCar = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnCar.setType(ContactActivator.NODE_NAME_PN_CAR);
+		InformationUnit pnIsdn = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnIsdn.setType(ContactActivator.NODE_NAME_PN_ISDN);
+		InformationUnit pnVideo = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnVideo.setType(ContactActivator.NODE_NAME_PN_VIDEO);
+		InformationUnit pnPcs = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnPcs.setType(ContactActivator.NODE_NAME_PN_PCS);
+		InformationUnit pnPager = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnPager.setType(ContactActivator.NODE_NAME_PN_PAGER);
+		InformationUnit pnOthers = InfomngmntFactory.eINSTANCE.createInformationUnit();
+		pnOthers.setType(ContactActivator.NODE_NAME_PN_OTHERS);
+		
 		returnValue.setType(ContactActivator.PASSWORD_INFO_ID);
 		returnValue.getChildValues().add(rawDataImage);
 		returnValue.getChildValues().add(rawDataLogo);
-		returnValue.getChildValues().add(origFilePath);
+		returnValue.getChildValues().add(origFilePath);		
+		returnValue.getChildValues().add(pnCar);
+		returnValue.getChildValues().add(pnFax);
+		returnValue.getChildValues().add(pnHome);
+		returnValue.getChildValues().add(pnIsdn);
+		returnValue.getChildValues().add(pnMailbox);
+		returnValue.getChildValues().add(pnMessanger);
+		returnValue.getChildValues().add(pnMobile);
+		returnValue.getChildValues().add(pnModem);
+		returnValue.getChildValues().add(pnOthers);
+		returnValue.getChildValues().add(pnPager);
+		returnValue.getChildValues().add(pnPcs);
+		returnValue.getChildValues().add(pnVideo);
+		returnValue.getChildValues().add(pnVoice);
+		returnValue.getChildValues().add(pnWork);
 		
 		return returnValue;
 	}

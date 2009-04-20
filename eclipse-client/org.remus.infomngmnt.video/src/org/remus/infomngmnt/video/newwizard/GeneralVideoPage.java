2ce24ddeaa2a301b8a7a150fcb5fe4e1a74789c7
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-04-20 19:44:34 +0000

    minor changes

---
+++ eclipse-client/org.remus.infomngmnt.video/src/org/remus/infomngmnt/video/newwizard/GeneralVideoPage.java
@@ -0,0 +1,165 @@
+package org.remus.infomngmnt.video.newwizard;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+
+import org.remus.infomngmnt.Category;
+import org.remus.infomngmnt.InfomngmntPackage;
+import org.remus.infomngmnt.InformationUnit;
+import org.remus.infomngmnt.InformationUnitListItem;
+import org.remus.infomngmnt.common.ui.databinding.BindingWidgetFactory;
+import org.remus.infomngmnt.common.ui.databinding.TextBindingWidget;
+import org.remus.infomngmnt.common.ui.image.ResourceManager;
+import org.remus.infomngmnt.core.model.EditingUtil;
+import org.remus.infomngmnt.core.model.InformationUtil;
+import org.remus.infomngmnt.core.operation.LoadFromFileRunnable;
+import org.remus.infomngmnt.mediaplayer.extension.IMediaPlayer;
+import org.remus.infomngmnt.mediaplayer.extension.IMediaPlayerExtensionService;
+import org.remus.infomngmnt.ui.UIPlugin;
+import org.remus.infomngmnt.ui.newwizards.GeneralPage;
+import org.remus.infomngmnt.video.VideoActivator;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class GeneralVideoPage extends GeneralPage {
+
+	private Text fileNameText;
+	private Button browseButton;
+	protected String tmpText;
+
+	private EditingDomain editingDomain;
+	private Text mediaTypeText;
+
+	public GeneralVideoPage(final Category category) {
+		super(category);
+		this.editingDomain = EditingUtil.getInstance().createNewEditingDomain();
+
+	}
+
+	public GeneralVideoPage(final InformationUnitListItem selection) {
+		super(selection);
+	}
+
+	@Override
+	public void createControl(final Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		container.setLayout(new GridLayout());
+		setTitle("New Video");
+		setMessage("This wizard enables you to create a new video from a file.");
+		setImageDescriptor(ResourceManager.getPluginImageDescriptor(VideoActivator.getDefault(),
+				"icons/iconexperience/video_wizard_title.png"));
+
+		doCreateParentElementGroup(container);
+		Group group = new Group(container, SWT.NONE);
+		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		group.setLayout(new GridLayout(3, false));
+		group.setText("Name && File");
+		doCreateNameElements(group);
+
+		GridData gd_nameText = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		gd_nameText.horizontalSpan = 2;
+		this.nameText.setLayoutData(gd_nameText);
+
+		final Label mediaTypeLabel = new Label(group, SWT.NONE);
+		mediaTypeLabel.setText("Media-Type");
+
+		this.mediaTypeText = new Text(group, SWT.BORDER);
+		this.mediaTypeText.setEditable(false);
+		gd_nameText = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		gd_nameText.horizontalSpan = 2;
+		this.mediaTypeText.setLayoutData(gd_nameText);
+		/*
+		 * we check if we have already set data.
+		 */
+		InformationUnit rawData = InformationUtil.getChildByType(this.unit,
+				VideoActivator.NODE_NAME_RAWDATA);
+		if (rawData.getBinaryValue() == null) {
+			final Label nameLabel = new Label(group, SWT.NONE);
+			nameLabel.setText("File");
+			this.fileNameText = new Text(group, SWT.BORDER);
+			gd_nameText = new GridData(SWT.FILL, SWT.CENTER, true, false);
+			gd_nameText.horizontalSpan = 2;
+			this.fileNameText.setLayoutData(gd_nameText);
+
+			this.browseButton = new Button(group, SWT.PUSH);
+			this.browseButton.setText("Browse...");
+			this.browseButton.addListener(SWT.Selection, new Listener() {
+
+				public void handleEvent(final Event event) {
+					FileDialog fd = new FileDialog(getShell());
+					fd.setFilterExtensions(new String[] { "*.*" });
+					fd.setFilterNames(new String[] { "Videos" });
+					String open = fd.open();
+					if (open != null) {
+						GeneralVideoPage.this.fileNameText.setText(open);
+						LoadFromFileRunnable lffr = new LoadFromFileRunnable(open, InformationUtil
+								.getChildByType(GeneralVideoPage.this.unit,
+										VideoActivator.NODE_NAME_RAWDATA),
+								GeneralVideoPage.this.editingDomain);
+						try {
+							getContainer().run(true, true, lffr);
+						} catch (InvocationTargetException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+
+			});
+		}
+
+		doCreatePropertiesGroup(container);
+		initDatabinding();
+		presetValues();
+		initValidation();
+		setControl(container);
+	}
+
+	@Override
+	protected void initDatabinding() {
+		super.initDatabinding();
+
+		TextBindingWidget mediaBinding = BindingWidgetFactory.createTextBinding(this.mediaTypeText,
+				this.ctx, this.editingDomain);
+		mediaBinding.bindModel(InformationUtil.getChildByType(this.unit,
+				VideoActivator.NODE_NAME_MEDIATYPE),
+				InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
+		this.fileNameText.addListener(SWT.Modify, new Listener() {
+			public void handleEvent(final Event event) {
+				IMediaPlayerExtensionService playerExtensionService = UIPlugin.getDefault()
+						.getService(IMediaPlayerExtensionService.class);
+				String type = new Path(GeneralVideoPage.this.fileNameText.getText())
+						.getFileExtension().toLowerCase();
+				IMediaPlayer playerByType = playerExtensionService.getPlayerByType(type);
+				InformationUnit mediaTypeNode = InformationUtil.getChildByType(
+						GeneralVideoPage.this.unit, VideoActivator.NODE_NAME_MEDIATYPE);
+				if (playerByType != null) {
+					mediaTypeNode.setStringValue(type);
+				} else {
+					mediaTypeNode.eUnset(InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
+				}
+				String name = new Path(GeneralVideoPage.this.fileNameText.getText())
+						.removeFileExtension().lastSegment();
+				GeneralVideoPage.this.nameText.setText(name);
+			}
+		});
+
+	}
+}

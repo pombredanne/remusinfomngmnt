f7f2e4478fc9fba9b711b8cddcd6923b85c6d6e4
jhartwig <jhartwig@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-04-22 10:41:15 +0000

    - create ContactActivator and ContactCreationFactory for address - minor changes

--- trunk/eclipse-client/org.remus.infomngmnt.contact/src/org/remus/infomngmnt/contact/ContactActivator.java
+++ trunk/eclipse-client/org.remus.infomngmnt.contact/src/org/remus/infomngmnt/contact/ContactActivator.java
@@ -61,9 +61,60 @@ public class ContactActivator extends AbstractUIPlugin {
 	public static final String NODE_NAME_PN_PAGER = "pnPager"; //$NON-NLS-1$
 	public static final String NODE_NAME_PN_OTHERS = "pnOthers"; //$NON-NLS-1$
 	
+	// group address
+	public static final String NODE_ADDRESS_WORK_STREET = "addWorkStreet"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_WORK_POST_OFFICE_BOX = "addWorkPob"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_WORK_LOCALITY = "addWorkLocality"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_WORK_REGION = "addWorkRegion"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_WORK_POSTAL = "addWorkPostal"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_WORK_COUNTRY = "addWorkCountry"; //$NON-NLS-1$
 	
+	public static final String NODE_ADDRESS_HOME_STREET = "addHomeStreet"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_HOME_POST_OFFICE_BOX = "addHomePob"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_HOME_LOCALITY = "addHomeLocality"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_HOME_REGION = "addHomeRegion"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_HOME_POSTAL = "addHomePostal"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_HOME_COUNTRY = "addHomeCountry"; //$NON-NLS-1$
+	
+	public static final String NODE_ADDRESS_INTERNAT_STREET = "addInternatStreet"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_INTERNAT_POST_OFFICE_BOX = "addInternatPob"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_INTERNAT_LOCALITY = "addInternatLocality"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_INTERNAT_REGION = "addInternatRegion"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_INTERNAT_POSTAL = "addInternatPostal"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_INTERNAT_COUNTRY = "addInternatCountry"; //$NON-NLS-1$
+	
+	public static final String NODE_ADDRESS_POSTAL_STREET = "addPostalStreet"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_POSTAL_POST_OFFICE_BOX = "addPostalPob"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_POSTAL_LOCALITY = "addPostalLocality"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_POSTAL_REGION = "addPostalRegion"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_POSTAL_POSTAL = "addPostalPostal"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_POSTAL_COUNTRY = "addPostalCountry"; //$NON-NLS-1$
+	
+	public static final String NODE_ADDRESS_PARCEL_STREET = "addParcelStreet"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_PARCEL_POST_OFFICE_BOX = "addParcelPob"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_PARCEL_LOCALITY = "addParcelLocality"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_PARCEL_REGION = "addParcelRegion"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_PARCEL_POSTAL = "addParcelPostal"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_PARCEL_COUNTRY = "addParcelCountry"; //$NON-NLS-1$
+	
+	public static final String NODE_ADDRESS_DOMESTIC_STREET = "addDomesticStreet"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_DOMESTIC_POST_OFFICE_BOX = "addDomesticPob"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_DOMESTIC_LOCALITY = "addDomesticLocality"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_DOMESTIC_REGION = "addDomesticRegion"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_DOMESTIC_POSTAL = "addDomesticPostal"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_DOMESTIC_COUNTRY = "addDomesticCountry"; //$NON-NLS-1$
+	
+	public static final String NODE_ADDRESS_OTHER_STREET = "addOtherStreet"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_OTHER_POST_OFFICE_BOX = "addOtherPob"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_OTHER_LOCALITY = "addOtherLocality"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_OTHER_REGION = "addOtherRegion"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_OTHER_POSTAL = "addOtherPostal"; //$NON-NLS-1$
+	public static final String NODE_ADDRESS_OTHER_COUNTRY = "addOtherCountry"; //$NON-NLS-1$
+	
+
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.remus.infomngmnt.contact";
+
 
 	// The shared instance
 	private static ContactActivator plugin;

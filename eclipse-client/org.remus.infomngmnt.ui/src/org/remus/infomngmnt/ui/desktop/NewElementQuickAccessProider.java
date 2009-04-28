58fd0931a1f66b433e19c87f00ddc4a600177896
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-04-28 11:03:14 +0000

    fixed RIMCORE-7

--- eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/desktop/NewElementQuickAccessProider.java
+++ eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/desktop/NewElementQuickAccessProider.java
@@ -24,16 +24,18 @@ import org.remus.infomngmnt.RuleAction;
 import org.remus.infomngmnt.RuleResult;
 import org.remus.infomngmnt.common.ui.quickaccess.QuickAccessElement;
 import org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider;
+import org.remus.infomngmnt.core.model.IdFactory;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
  */
 public class NewElementQuickAccessProider extends QuickAccessProvider {
 
-	public NewElementQuickAccessProider(RuleResult result) {
+	public NewElementQuickAccessProider(final RuleResult result) {
 		super();
 		this.result = result;
 	}
+
 	private Map<String, QuickAccessElement> action2idMap;
 
 	public Map<String, QuickAccessElement> getAction2idMap() {
@@ -41,50 +43,65 @@ public class NewElementQuickAccessProider extends QuickAccessProvider {
 			this.action2idMap = new HashMap<String, QuickAccessElement>();
 			EList<RuleAction> actions = this.result.getActions();
 			for (RuleAction ruleAction : actions) {
-				this.action2idMap.put(
-						ruleAction.getInfoTypeId(),
-						new NewElementQuickAccessElement(this,ruleAction,this.result.getValue()));
+				this.action2idMap.put(IdFactory.createId(), new NewElementQuickAccessElement(this,
+						ruleAction, this.result.getValue()));
 			}
 		}
 		return this.action2idMap;
 	}
+
 	private final RuleResult result;
 
-	/* (non-Javadoc)
-	 * @see org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#getElementForId(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#
+	 * getElementForId(java.lang.String)
 	 */
 	@Override
-	public QuickAccessElement getElementForId(String id) {
+	public QuickAccessElement getElementForId(final String id) {
 		return getAction2idMap().get(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#getElements()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#getElements
+	 * ()
 	 */
 	@Override
 	public QuickAccessElement[] getElements() {
 		return getAction2idMap().values().toArray(new QuickAccessElement[0]);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#getId()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#getId()
 	 */
 	@Override
 	public String getId() {
 		return "test";
 	}
 
-	/* (non-Javadoc)
-	 * @see org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#getImageDescriptor()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#
+	 * getImageDescriptor()
 	 */
 	@Override
 	public ImageDescriptor getImageDescriptor() {
-		return WorkbenchImages
-		.getImageDescriptor(IWorkbenchGraphicConstants.IMG_OBJ_NODE);
+		return WorkbenchImages.getImageDescriptor(IWorkbenchGraphicConstants.IMG_OBJ_NODE);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#getName()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.remus.infomngmnt.common.ui.quickaccess.QuickAccessProvider#getName()
 	 */
 	@Override
 	public String getName() {

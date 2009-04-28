4e0c4dde1d2cbccb8a47e045e52248775cdb7a0f
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-04-28 11:03:29 +0000

    fixed RIMCORE-7

--- eclipse-client/org.remus.infomngmnt.image/src/org/remus/infomngmnt/image/preferences/ImageCreationPreferencePage.java
+++ eclipse-client/org.remus.infomngmnt.image/src/org/remus/infomngmnt/image/preferences/ImageCreationPreferencePage.java
@@ -12,6 +12,19 @@
 
 package org.remus.infomngmnt.image.preferences;
 
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.emf.databinding.EMFDataBindingContext;
+import org.eclipse.emf.databinding.EMFObservables;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.swt.SWT;
+
+import org.remus.infomngmnt.InfomngmntFactory;
+import org.remus.infomngmnt.InfomngmntPackage;
+import org.remus.infomngmnt.InformationUnit;
+import org.remus.infomngmnt.RuleValue;
+import org.remus.infomngmnt.core.model.InformationUtil;
 import org.remus.infomngmnt.ui.extension.AbstractCreationPreferencePage;
 
 /**
@@ -19,11 +32,45 @@ import org.remus.infomngmnt.ui.extension.AbstractCreationPreferencePage;
  */
 public class ImageCreationPreferencePage extends AbstractCreationPreferencePage {
 
+	private final EMFDataBindingContext ctx;
+	private InformationUnit preDefCategory;
+	private InformationUnit preDefName;
+
 	/**
 	 * 
 	 */
 	public ImageCreationPreferencePage() {
-		// nothing special
+		this.ctx = new EMFDataBindingContext();
 	}
 
+	@Override
+	public void setValues(final RuleValue value, final EditingDomain domain) {
+		this.ctx.dispose();
+		super.setValues(value, domain);
+		this.preDefCategory = InformationUtil.getChildByType(value, NODENAME_PREDEFINED_CATEGORY);
+		if (this.preDefCategory == null) {
+			this.preDefCategory = InfomngmntFactory.eINSTANCE.createRuleValue();
+			this.preDefCategory.setType(NODENAME_PREDEFINED_CATEGORY);
+			value.getChildValues().add(this.preDefCategory);
+		}
+		this.preDefName = InformationUtil.getChildByType(value, NODENAME_PREDEFINED_NAME);
+		if (this.preDefName == null) {
+			this.preDefName = InfomngmntFactory.eINSTANCE.createRuleValue();
+			this.preDefName.setType(NODENAME_PREDEFINED_NAME);
+			value.getChildValues().add(this.preDefName);
+		}
+	}
+
+	@Override
+	public void bindValuesToUi() {
+		ISWTObservableValue swtCategory = SWTObservables.observeText(this.predefCategoryText,
+				SWT.Modify);
+		IObservableValue emfCategory = EMFObservables.observeValue(this.preDefCategory,
+				InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
+		ISWTObservableValue swtName = SWTObservables.observeText(this.predefNameText, SWT.Modify);
+		IObservableValue emfName = EMFObservables.observeValue(this.preDefName,
+				InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
+		this.ctx.bindValue(swtCategory, emfCategory, null, null);
+		this.ctx.bindValue(swtName, emfName, null, null);
+	}
 }

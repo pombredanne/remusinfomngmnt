3fc741d5d22bdf8acc33407083c25dc3cad580f0
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-05-01 11:05:10 +0000

    fixed RIMINFOTYPES-7

---
+++ eclipse-client/org.remus.infomngmnt.mediaplayer/src/org/remus/infomngmnt/mediaplayer/avi/AviPlayer.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.mediaplayer.avi;
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.eclipse.core.runtime.IPath;
+
+import org.remus.infomngmnt.mediaplayer.extension.AbstractMediaPlayer;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class AviPlayer extends AbstractMediaPlayer {
+
+	/**
+	 * 
+	 */
+	public AviPlayer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.remus.infomngmnt.mediaplayer.extension.IMediaPlayer#buildHtml(org
+	 * .eclipse.core.runtime.IPath, int, int, java.util.Map)
+	 */
+	public String buildHtml(final IPath mediaFilePath, final int widht, final int height,
+			final Map<String, String> options) {
+		return "<p align=\"center\"><embed src=\""
+				+ StringEscapeUtils.escapeHtml(mediaFilePath.toOSString())
+				+ "\" autoplay=\"false\"\r\n" + "fullscreen=\"full\"\r\n"
+				+ "quitwhendone=\"true\"height=\"" + (height == 0 ? 300 : height) + "\" width=\""
+				+ (widht == 0 ? 480 : widht) + "\"></p>";
+	}
+
+}

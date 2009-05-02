cd03c83bcff11b624500dc306f47facbc8e089cc
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-05-02 09:43:57 +0000

    small bugfix

--- eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/UIPlugin.java
+++ eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/UIPlugin.java
@@ -1,10 +1,10 @@
 package org.remus.infomngmnt.ui;
 
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
-import org.remus.infomngmnt.common.ui.UIUtil;
 import org.remus.infomngmnt.common.ui.image.CommonImageRegistry;
 
 /**
@@ -17,7 +17,7 @@ public class UIPlugin extends AbstractUIPlugin {
 
 	// The shared instance
 	private static UIPlugin plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -26,21 +26,22 @@ public class UIPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	@Override
 	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		/*
-		 * This is a bit ugly. Problem is
-		 * that if the first access on the image-registry
-		 * comes from a non-ui thread the initialization fails.
+		 * This is a bit ugly. Problem is that if the first access on the
+		 * image-registry comes from a non-ui thread the initialization fails.
 		 * 
-		 * This is not good, especially because in this image-registry
-		 * there are decorator-icons which are loaded very early and
-		 * in a non-ui thread.
+		 * This is not good, especially because in this image-registry there are
+		 * decorator-icons which are loaded very early and in a non-ui thread.
 		 */
-		UIUtil.getDisplay().asyncExec(new Runnable() {
+		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
 				CommonImageRegistry.getInstance();
 			}
@@ -50,7 +51,10 @@ public class UIPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	@Override
 	public void stop(final BundleContext context) throws Exception {
@@ -60,15 +64,16 @@ public class UIPlugin extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static UIPlugin getDefault() {
 		return plugin;
 	}
-	
+
 	public <T> T getService(final Class<T> serviceClass) {
-		ServiceReference serviceReference = getBundle().getBundleContext().getServiceReference(serviceClass.getName());
+		ServiceReference serviceReference = getBundle().getBundleContext().getServiceReference(
+				serviceClass.getName());
 		if (serviceReference != null) {
 			Object service = getBundle().getBundleContext().getService(serviceReference);
 			return (T) service;

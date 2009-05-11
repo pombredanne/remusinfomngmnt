c0a931d017610e630fda460fd1e0bcb8135c04c1
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-05-11 15:21:46 +0000

    initial version

---
+++ eclipse-client/org.remus.infomngmnt.favoritesearch/src/org/remus/infomngmnt/favoritesearch/FavoriteSearchActivator.java
@@ -0,0 +1,63 @@
+package org.remus.infomngmnt.favoritesearch;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+import org.remus.infomngmnt.favoritesearch.service.FavSearchHandler;
+import org.remus.infomngmnt.search.service.IFavoriteSearchHandler;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class FavoriteSearchActivator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.remus.infomngmnt.favoritesearch";
+
+	// The shared instance
+	private static FavoriteSearchActivator plugin;
+
+	/**
+	 * The constructor
+	 */
+	public FavoriteSearchActivator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
+		context.registerService(IFavoriteSearchHandler.class.getName(), new FavSearchHandler(),
+				null);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void stop(final BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static FavoriteSearchActivator getDefault() {
+		return plugin;
+	}
+
+}

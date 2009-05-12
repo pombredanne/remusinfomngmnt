8a774f75d2ce6e476d5417bcd087fd26d849f573
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-05-12 14:32:13 +0000

    making editor aware of changes outside the editor + refreshing the databindings

--- eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/ui/FormPage.java
+++ eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/ui/FormPage.java
@@ -18,7 +18,7 @@ public class FormPage extends AbstractInformationFormPage {
 	private Text styledText;
 
 	@Override
-	protected void createFormContent(final IManagedForm managedForm) {
+	protected void renderPage(final IManagedForm managedForm) {
 		FormToolkit toolkit = managedForm.getToolkit();
 		ScrolledForm form = managedForm.getForm();
 		Composite body = form.getBody();
@@ -28,10 +28,16 @@ public class FormPage extends AbstractInformationFormPage {
 		this.styledText = toolkit.createText(body,
 				"", SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL | SWT.H_SCROLL); //$NON-NLS-1$
 		this.styledText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		addDirtyOnTextModifyListener(this.styledText,
-				InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
+
 		toolkit.adapt(this.styledText, true, true);
 
+	}
+
+	@Override
+	public void bindValuesToUi() {
+		addDirtyOnTextModifyListener(this.styledText,
+				InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
+		super.bindValuesToUi();
 	}
 
 	@Override

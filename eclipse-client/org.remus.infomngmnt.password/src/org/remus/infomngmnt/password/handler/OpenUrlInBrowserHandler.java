7e67c096d4c03e923e4826005cf96d1a2ea2e2f6
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-05-15 15:42:31 +0000

    actions

---
+++ eclipse-client/org.remus.infomngmnt.password/src/org/remus/infomngmnt/password/handler/OpenUrlInBrowserHandler.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.password.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.program.Program;
+
+import org.remus.infomngmnt.InformationUnit;
+import org.remus.infomngmnt.core.model.InformationUtil;
+import org.remus.infomngmnt.password.PasswordPlugin;
+import org.remus.infomngmnt.ui.handlerutil.InformationHandlerUtil;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class OpenUrlInBrowserHandler extends AbstractHandler {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.
+	 * ExecutionEvent)
+	 */
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		InformationUnit unit = InformationHandlerUtil.getInformationUnitFromExecutionEvent(event);
+		if (unit != null) {
+			Program.launch(InformationUtil.getChildByType(unit, PasswordPlugin.NODE_URL)
+					.getStringValue());
+		}
+		return null;
+	}
+
+}

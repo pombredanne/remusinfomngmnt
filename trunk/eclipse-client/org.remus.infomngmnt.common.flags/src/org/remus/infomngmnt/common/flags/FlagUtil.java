607fe53f3bcc0d55c40347111482c86f3a013f1b
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-05-25 12:11:03 +0000

    + minor changes

---
+++ trunk/eclipse-client/org.remus.infomngmnt.common.flags/src/org/remus/infomngmnt/common/flags/FlagUtil.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.common.flags;
+
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class FlagUtil {
+
+	public static String getPathByIsoCode(final String isoCode) {
+		URL find = FileLocator.find(Activator.getDefault().getBundle(), new Path(
+				"iconexperience/flags/" + isoCode + ".png"), null);
+		if (find != null) {
+			return "iconexperience/flags/" + isoCode + ".png";
+		} else {
+			return "iconexperience/flags/flag_generic.png";
+		}
+	}
+
+}

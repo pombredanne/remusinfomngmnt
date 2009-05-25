a85c71a1aadbc43c14c7d83d258fb0c6c428dcf3
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-05-25 12:11:18 +0000

    + minor changes

--- eclipse-client/org.remus.infomngmnt.geodata/src/org/remus/infomngmnt/geodata/preferences/GeoDataEditorPreferencePage.java
+++ eclipse-client/org.remus.infomngmnt.geodata/src/org/remus/infomngmnt/geodata/preferences/GeoDataEditorPreferencePage.java
@@ -18,12 +18,21 @@ package org.remus.infomngmnt.geodata.preferences;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+
 import org.remus.infomngmnt.geodata.GeoDataActivator;
 
-public class GeoDataEditorPreferencePage extends FieldEditorPreferencePage
-		implements IWorkbenchPreferencePage {
+public class GeoDataEditorPreferencePage extends FieldEditorPreferencePage implements
+		IWorkbenchPreferencePage {
 
 	private StringFieldEditor sfe;
 
@@ -40,11 +49,27 @@ public class GeoDataEditorPreferencePage extends FieldEditorPreferencePage
 	@Override
 	protected void createFieldEditors() {
 		{
-			this.sfe = new StringFieldEditor(
-					GeoDataPreferenceInitializer.GOOGLE_API_KEY,
-					"Google Maps API Key:", getFieldEditorParent());
+			this.sfe = new StringFieldEditor(GeoDataPreferenceInitializer.GOOGLE_API_KEY,
+					"Google Maps API Key:", 35, getFieldEditorParent());
 			addField(this.sfe);
+
 		}
+	}
+
+	@Override
+	protected Control createContents(final Composite parent) {
+		// TODO Auto-generated method stub
+		Composite createContents = (Composite) super.createContents(parent);
+		Link link = new Link(createContents, SWT.NONE);
+		link.setText("<a>Get a Google Maps API Key</a>");
+		link.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				Program.launch("http://code.google.com/apis/maps/signup.html");
+			}
+		});
+		link.setLayoutData(new GridData(SWT.BEGINNING, SWT.BEGINNING, true, false));
+		return createContents;
 	}
 
 	/**

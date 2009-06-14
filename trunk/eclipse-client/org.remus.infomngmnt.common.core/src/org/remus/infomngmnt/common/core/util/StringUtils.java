c7b6a78c04a6321fa7655083748197b005b1fecc
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-06-14 12:09:46 +0000

    + util for replacing matchings by a regular expression

--- trunk/eclipse-client/org.remus.infomngmnt.common.core/src/org/remus/infomngmnt/common/core/util/StringUtils.java
+++ trunk/eclipse-client/org.remus.infomngmnt.common.core/src/org/remus/infomngmnt/common/core/util/StringUtils.java
@@ -12,6 +12,9 @@
 
 package org.remus.infomngmnt.common.core.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
  */
@@ -19,6 +22,33 @@ public class StringUtils {
 
 	public static String join(final String... string) {
 		return org.apache.commons.lang.StringUtils.join(string);
+	}
+
+	public static String surroundMatchingInString(String sourceString, final String[] matchings,
+			final String pre[], final String post[]) {
+		if (matchings.length != pre.length && matchings.length != post.length) {
+			throw new IllegalArgumentException("Wrong input data");
+		}
+		for (int i = 0, n = matchings.length; i < n; i++) {
+			String patternString = matchings[i];
+			if (patternString.trim().length() > 0) {
+				Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
+				Matcher matcher = pattern.matcher(sourceString);
+				int matchcount = 0;
+				while (matcher.find()) {
+					int shift = (pre[i].length() + post[i].length()) * matchcount;
+					StringBuilder sb = new StringBuilder();
+					sourceString = sb.append(sourceString.substring(0, matcher.start() + shift))
+							.append(pre[i]).append(
+									sourceString.substring(matcher.start() + shift, matcher.end()
+											+ shift)).append(post[i]).append(
+									sourceString.substring(matcher.end() + shift, sourceString
+											.length())).toString();
+					matchcount++;
+				}
+			}
+		}
+		return sourceString;
 	}
 
 }

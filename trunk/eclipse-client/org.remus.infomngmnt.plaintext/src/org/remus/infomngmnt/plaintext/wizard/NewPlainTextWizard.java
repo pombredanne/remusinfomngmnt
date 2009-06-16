c3a6b7763356d781c3bba1dfa7152c0d98f93326
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-06-16 22:55:09 +0000

    RIMINFOTYPES-33

--- trunk/eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/wizard/NewPlainTextWizard.java
+++ trunk/eclipse-client/org.remus.infomngmnt.plaintext/src/org/remus/infomngmnt/plaintext/wizard/NewPlainTextWizard.java
@@ -12,6 +12,7 @@
 
 package org.remus.infomngmnt.plaintext.wizard;
 
+import org.remus.infomngmnt.common.ui.image.ResourceManager;
 import org.remus.infomngmnt.plaintext.Activator;
 import org.remus.infomngmnt.ui.newwizards.NewInfoObjectWizard;
 
@@ -20,11 +21,23 @@ import org.remus.infomngmnt.ui.newwizards.NewInfoObjectWizard;
  */
 public class NewPlainTextWizard extends NewInfoObjectWizard {
 
+	public NewPlainTextWizard() {
+		super();
+		setWindowTitle("New unformatted text");
+	}
+
 	@Override
 	protected String getInfoTypeId() {
 		return Activator.INFO_TYPE_ID;
 	}
 
-	
+	@Override
+	public void addPages() {
+		super.addPages();
+		this.page1.setTitle("New unformatted text");
+		this.page1.setMessage("This wizard enables you to create new unformatted text units");
+		this.page1.setImageDescriptor(ResourceManager.getPluginImageDescriptor(Activator
+				.getDefault(), "icons/iconexperience/new_wizard.png"));
+	}
 
 }

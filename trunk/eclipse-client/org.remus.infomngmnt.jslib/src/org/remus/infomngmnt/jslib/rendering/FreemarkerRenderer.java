870e43f4f89010f2589eeaab96590f16255e848b
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-07-21 20:57:48 +0000

    improvements on rendering html-output

--- trunk/eclipse-client/org.remus.infomngmnt.jslib/src/org/remus/infomngmnt/jslib/rendering/FreemarkerRenderer.java
+++ trunk/eclipse-client/org.remus.infomngmnt.jslib/src/org/remus/infomngmnt/jslib/rendering/FreemarkerRenderer.java
@@ -25,14 +25,14 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.forms.IFormColors;
 import org.xml.sax.InputSource;
 
+import org.remus.infomngmnt.jslib.StyleProvider;
+import org.remus.infomngmnt.jslib.TemplateLocation;
+import org.remus.infomngmnt.jslib.extension.CheckResourceReferenceJob;
+
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.Template;
 import freemarker.template.TemplateModelException;
-
-import org.remus.infomngmnt.jslib.StyleProvider;
-import org.remus.infomngmnt.jslib.TemplateLocation;
-import org.remus.infomngmnt.jslib.extension.CheckResourceReferenceJob;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
@@ -77,17 +77,80 @@ public class FreemarkerRenderer {
 		}
 	}
 
+	/**
+	 * Creates an Html {@link OutputStream} based on the given Template-
+	 * {@link InputStream} and data maps.
+	 * 
+	 * @param templateName
+	 *            the name of the template which may be already cached in the
+	 *            {@link Configuration}
+	 * @param template
+	 *            the template as strem
+	 * @param out
+	 *            the outputstream
+	 * @param additionals
+	 *            additional map. Values that are used in the template but are
+	 *            not directly present in the information unit.
+	 * @param datastructure
+	 *            the datastructure. All values
+	 * @param dynamicStructure
+	 *            the dynamic structure. This is a second map which can be used
+	 *            for lists.
+	 * @throws RenderingException
+	 */
+	public void process(final String templateName, final InputStream template,
+			final OutputStream out, final Map<String, ? extends Object> additionals,
+			final Map<String, ? extends Object> datastructure,
+			final Map<String, ? extends Object> dynamicStructure) throws RenderingException {
+		try {
+			Template freemarkerTemplate;
+			freemarkerTemplate = new Template(templateName, new InputStreamReader(template),
+					this.cfg);
+
+			Map<String, Object> root = new HashMap<String, Object>();
+
+			root.put("additionals", additionals);
+			root.put("data", datastructure);
+			root.put("dynamic", dynamicStructure);
+
+			Writer writer = new OutputStreamWriter(out, "UTF-8");
+			freemarkerTemplate.process(root, writer);
+		} catch (Exception e) {
+			Status status = new Status(IStatus.ERROR, TemplateLocation.BUNDLE_ID, 300,
+					"Error while serializing object", e);
+			throw new RenderingException(status);
+		}
+
+	}
+
+	/**
+	 * Renders a Freemarker Template.
+	 * 
+	 * @param templateName
+	 * @param template
+	 * @param xmlFile
+	 * @param out
+	 * @param additionals
+	 * @throws RenderingException
+	 * 
+	 * @deprecated This method gives the freemarker template a xml node for
+	 *             rendering. This is deprecated, use
+	 *             {@link #process(String, InputStream, OutputStream, Map, Map, Map)}
+	 *             instead.
+	 */
+	@Deprecated
 	public void process(final String templateName, final InputStream template,
 			final InputStream xmlFile, final OutputStream out,
 			final Map<String, ? extends Object> additionals) throws RenderingException {
-
 		try {
 			Template freemarekerTemplate;
 			freemarekerTemplate = new Template(templateName, new InputStreamReader(template),
 					this.cfg);
 
 			Map<String, Object> root = new HashMap<String, Object>();
-			root.put("doc", freemarker.ext.dom.NodeModel.parse(new InputSource(xmlFile)));
+			if (xmlFile != null) {
+				root.put("doc", freemarker.ext.dom.NodeModel.parse(new InputSource(xmlFile)));
+			}
 			root.put("additionals", additionals);
 
 			Writer writer = new OutputStreamWriter(out, "UTF-8");

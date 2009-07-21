80c8b4f88993847d1669026194b5ac602dda316f
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-07-21 21:03:39 +0000

    initial version

---
+++ trunk/eclipse-client/org.remus.infomngmnt.mail/src/org/remus/infomngmnt/mail/ui/AttachmentsEditPage.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.mail.ui;
+
+import org.eclipse.ui.forms.IManagedForm;
+
+import org.remus.infomngmnt.ui.extension.AbstractInformationFormPage;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class AttachmentsEditPage extends AbstractInformationFormPage {
+
+	/* (non-Javadoc)
+	 * @see org.remus.infomngmnt.ui.extension.AbstractInformationFormPage#renderPage(org.eclipse.ui.forms.IManagedForm)
+	 */
+	@Override
+	protected void renderPage(IManagedForm managedForm) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

9a0314d9ce682b0b6f8f45fe9bcca46648d08661
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-07-27 08:21:18 +0000

    RIMCONNECTORS-9

--- eclipse-client/org.remus.infomngmnt.connector.rss/src/org/remus/infomngmnt/connector/rss/jobs/DeleteOldEntriesJob.java
+++ eclipse-client/org.remus.infomngmnt.connector.rss/src/org/remus/infomngmnt/connector/rss/jobs/DeleteOldEntriesJob.java
@@ -12,46 +12,44 @@
 
 package org.remus.infomngmnt.connector.rss.jobs;
 
-import java.util.Date;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.query.conditions.eobjects.EObjectCondition;
 import org.eclipse.emf.query.statements.FROM;
 import org.eclipse.emf.query.statements.IQueryResult;
 import org.eclipse.emf.query.statements.SELECT;
 import org.eclipse.emf.query.statements.WHERE;
 
-import org.remus.infomngmnt.Category;
-import org.remus.infomngmnt.InfomngmntFactory;
 import org.remus.infomngmnt.InfomngmntPackage;
+import org.remus.infomngmnt.InformationUnit;
+import org.remus.infomngmnt.InformationUnitListItem;
 import org.remus.infomngmnt.Notification;
 import org.remus.infomngmnt.RemoteRepository;
 import org.remus.infomngmnt.connector.rss.RssActivator;
+import org.remus.infomngmnt.core.commands.CommandFactory;
 import org.remus.infomngmnt.core.jobs.AbstractJob;
 import org.remus.infomngmnt.core.model.ApplicationModelPool;
 import org.remus.infomngmnt.core.services.IRepositoryService;
-import org.remus.infomngmnt.core.sync.SyncUtil;
 import org.remus.infomngmnt.ui.UIPlugin;
+import org.remus.infomngmnt.util.EditingUtil;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
  */
-public class RefreshRssJob extends AbstractJob {
-
-	private final Map<String, Date> lastRefresh;
+public class DeleteOldEntriesJob extends AbstractJob {
 
 	/**
 	 * 
 	 */
-	public RefreshRssJob() {
-		this.lastRefresh = new HashMap<String, Date>();
+	public DeleteOldEntriesJob() {
+		// TODO Auto-generated constructor stub
 	}
 
 	/*
@@ -65,54 +63,41 @@ public class RefreshRssJob extends AbstractJob {
 	public List<Notification> run(final IProgressMonitor monitor) {
 		EList<RemoteRepository> repositories = UIPlugin.getDefault().getService(
 				IRepositoryService.class).getRepositories().getRepositories();
-		Notification returnValue = InfomngmntFactory.eINSTANCE.createNotification();
 		for (final RemoteRepository remoteRepository : repositories) {
 			if (RssActivator.REPOSITORY_ID.equals(remoteRepository.getRepositoryTypeId())) {
-				boolean refresh = false;
-				if (this.lastRefresh.get(remoteRepository.getId()) != null) {
-					Date date = this.lastRefresh.get(remoteRepository.getId());
-					int parseInt = Integer.parseInt(remoteRepository.getOptions().get(
-							RssActivator.REPOSITORY_OPTIONS_REFRESH_INTERVAL));
-					if (date.getTime() + (parseInt * 60 * 1000) > System.currentTimeMillis()) {
-						refresh = true;
+				final int parseInt = Integer.parseInt(remoteRepository.getOptions().get(
+						RssActivator.REPOSITORY_OPTIONS_DELETE_AFTER_X_DAY));
+				SELECT select = new SELECT(new FROM(ApplicationModelPool.getInstance().getModel()
+						.getRootCategories()), new WHERE(new EObjectCondition() {
+					@Override
+					public boolean isSatisfied(final EObject arg0) {
+						return arg0.eClass() == InfomngmntPackage.Literals.INFORMATION_UNIT_LIST_ITEM
+								&& ((InformationUnitListItem) arg0).getSynchronizationMetaData() != null
+								&& remoteRepository.getId().equals(
+										((InformationUnitListItem) arg0)
+												.getSynchronizationMetaData().getRepositoryId())
+								&& ((InformationUnit) ((InformationUnitListItem) arg0)
+										.getAdapter(InformationUnit.class)).getCreationDate()
+										.getTime()
+										+ (parseInt * 1000 * 60 * 60 * 24) > System
+										.currentTimeMillis();
 					}
-				} else {
-					refresh = true;
+				}));
+				IQueryResult execute = select.execute();
+				Set<? extends EObject> eObjects = execute.getEObjects();
+				CompoundCommand compoundCommand = new CompoundCommand();
+				EditingDomain domain = EditingUtil.getInstance().createNewEditingDomain();
+				for (EObject eObject : eObjects) {
+					compoundCommand.append(CommandFactory.DELETE_INFOUNIT(Collections
+							.singletonList((InformationUnitListItem) eObject), domain));
 				}
-				if (refresh) {
-					SELECT select = new SELECT(new FROM(ApplicationModelPool.getInstance()
-							.getModel().getRootCategories()), new WHERE(new EObjectCondition() {
-						@Override
-						public boolean isSatisfied(final EObject arg0) {
-							return arg0.eClass() == InfomngmntPackage.Literals.CATEGORY
-									&& ((Category) arg0).getSynchronizationMetaData() != null
-									&& remoteRepository.getId().equals(
-											((Category) arg0).getSynchronizationMetaData()
-													.getRepositoryId());
-						}
-					}));
-					IQueryResult execute = select.execute();
-					Set<? extends EObject> eObjects = execute.getEObjects();
-					for (EObject eObject : eObjects) {
-						try {
-							Notification synchronizeCategory = SyncUtil.synchronizeCategory(
-									(Category) eObject, monitor);
-							returnValue.getChildren().addAll(synchronizeCategory.getChildren());
-
-						} catch (CoreException e) {
-							// do nothing
-						}
-					}
-					this.lastRefresh.put(remoteRepository.getId(), new Date());
+				if (compoundCommand.canExecute()) {
+					domain.getCommandStack().execute(compoundCommand);
 				}
+				domain.getCommandStack().flush();
 			}
 		}
-		return returnValue.getChildren();
-	}
-
-	@Override
-	public int getInterval() {
-		return 1;
+		return null;
 	}
 
 }

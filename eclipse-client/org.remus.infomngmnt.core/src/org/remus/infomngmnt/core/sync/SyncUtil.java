08ae4250fdf5d76e8833b861020e27994bde2924
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-07-27 08:22:31 +0000

    minor changes

--- eclipse-client/org.remus.infomngmnt.core/src/org/remus/infomngmnt/core/sync/SyncUtil.java
+++ eclipse-client/org.remus.infomngmnt.core/src/org/remus/infomngmnt/core/sync/SyncUtil.java
@@ -12,19 +12,30 @@
 
 package org.remus.infomngmnt.core.sync;
 
+import java.util.Date;
 import java.util.Set;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.EMap;
+import org.eclipse.emf.compare.diff.metamodel.DiffModel;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.osgi.util.NLS;
 
 import org.remus.infomngmnt.AbstractInformationUnit;
 import org.remus.infomngmnt.Category;
+import org.remus.infomngmnt.ChangeSet;
 import org.remus.infomngmnt.ChangeSetItem;
+import org.remus.infomngmnt.InfomngmntFactory;
 import org.remus.infomngmnt.InformationUnit;
 import org.remus.infomngmnt.InformationUnitListItem;
+import org.remus.infomngmnt.Notification;
+import org.remus.infomngmnt.NotificationImportance;
+import org.remus.infomngmnt.Severity;
 import org.remus.infomngmnt.SynchronizableObject;
 import org.remus.infomngmnt.SynchronizationAction;
+import org.remus.infomngmnt.core.extension.IInfoType;
+import org.remus.infomngmnt.core.extension.InformationExtensionManager;
 import org.remus.infomngmnt.core.remote.IRepository;
 import org.remus.infomngmnt.core.remote.RemoteException;
 
@@ -82,6 +93,42 @@ public class SyncUtil {
 		InformationUnitListItem adapter = (InformationUnitListItem) item
 				.getAdapter(InformationUnitListItem.class);
 		return adapter.getSynchronizationMetaData().getRepositoryId();
+
+	}
+
+	public static Notification synchronizeCategory(final Category eObject,
+			final IProgressMonitor monitor) throws CoreException {
+		ChangeSetManager manager = new ChangeSetManager();
+		Notification returnValue = InfomngmntFactory.eINSTANCE.createNotification();
+		ChangeSet createChangeSet = manager.createChangeSet(eObject, monitor);
+		if (createChangeSet != null && createChangeSet.getChangeSetItems().size() > 0) {
+			ChangeSetItem changeSetItem = createChangeSet.getChangeSetItems().get(0);
+			DiffModel createDiffModel = manager.createDiffModel(changeSetItem, eObject);
+			manager.prepareSyncActions(createDiffModel.getOwnedElements(), changeSetItem, eObject);
+			manager.updateFromRemote(changeSetItem);
+			ChangeSetExecutor executor = new ChangeSetExecutor();
+			executor.synchronize(createDiffModel.getOwnedElements(), changeSetItem, monitor,
+					eObject);
+			Set<SynchronizableObject> keySet = changeSetItem.getSyncObjectActionMap().keySet();
+			for (SynchronizableObject synchronizableObject : keySet) {
+				Notification notification = InfomngmntFactory.eINSTANCE.createNotification();
+				InformationUnitListItem item = (InformationUnitListItem) synchronizableObject;
+				IInfoType infoType = InformationExtensionManager.getInstance().getInfoTypeByType(
+						item.getType());
+				if (infoType != null) {
+					notification.setImage(infoType.getImage());
+				}
+				notification.setTimeStamp(new Date());
+				notification.setMessage(NLS.bind("Updated \"{0}\"", item.getLabel()));
+				notification.getAffectedInfoUnitIds().add(item.getId());
+				notification.setImportance(NotificationImportance.MEDIUM);
+				// notification.setSource(synchronizableObject.getSynchronizationMetaData()
+				// .getRepositoryId());
+				notification.setSeverity(Severity.OK);
+				returnValue.getChildren().add(notification);
+			}
+		}
+		return returnValue;
 
 	}
 
--- eclipse-client/org.remus.infomngmnt.core/src/org/remus/infomngmnt/core/sync/SynchronizeItemsJob.java
+++ eclipse-client/org.remus.infomngmnt.core/src/org/remus/infomngmnt/core/sync/SynchronizeItemsJob.java
@@ -12,31 +12,21 @@
 
 package org.remus.infomngmnt.core.sync;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.compare.diff.metamodel.DiffModel;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.query.conditions.eobjects.EObjectCondition;
 import org.eclipse.emf.query.statements.FROM;
 import org.eclipse.emf.query.statements.SELECT;
 import org.eclipse.emf.query.statements.WHERE;
-import org.eclipse.osgi.util.NLS;
 
 import org.remus.infomngmnt.Category;
-import org.remus.infomngmnt.ChangeSet;
-import org.remus.infomngmnt.ChangeSetItem;
 import org.remus.infomngmnt.InfomngmntFactory;
 import org.remus.infomngmnt.InfomngmntPackage;
-import org.remus.infomngmnt.InformationUnitListItem;
 import org.remus.infomngmnt.Notification;
-import org.remus.infomngmnt.NotificationImportance;
-import org.remus.infomngmnt.Severity;
 import org.remus.infomngmnt.SynchronizableObject;
-import org.remus.infomngmnt.core.extension.IInfoType;
-import org.remus.infomngmnt.core.extension.InformationExtensionManager;
 import org.remus.infomngmnt.core.jobs.AbstractJob;
 import org.remus.infomngmnt.core.model.ApplicationModelPool;
 
@@ -87,52 +77,13 @@ public class SynchronizeItemsJob extends AbstractJob {
 				.AND(isSyncRootFolderCondition)));
 		Set<? extends EObject> eObjects = select.execute().getEObjects();
 		for (EObject eObject : eObjects) {
-			final ChangeSetManager manager = new ChangeSetManager();
-			Notification sync = InfomngmntFactory.eINSTANCE.createNotification();
-			int updateCount = 0;
 			try {
-				ChangeSet createChangeSet = manager.createChangeSet((Category) eObject, monitor);
-				if (createChangeSet != null && createChangeSet.getChangeSetItems().size() > 0) {
-					ChangeSetItem changeSetItem = createChangeSet.getChangeSetItems().get(0);
-					DiffModel createDiffModel = manager.createDiffModel(changeSetItem,
-							(Category) eObject);
-					manager.prepareSyncActions(createDiffModel.getOwnedElements(), changeSetItem,
-							(Category) eObject);
-					manager.updateFromRemote(changeSetItem);
-
-					sync.setMessage(NLS.bind("Updated {0} elements on item \"{1}\"", updateCount,
-							((Category) eObject).getLabel()));
-					ChangeSetExecutor executor = new ChangeSetExecutor();
-					executor.synchronize(createDiffModel.getOwnedElements(), changeSetItem,
-							monitor, (Category) eObject);
-					Set<SynchronizableObject> keySet = changeSetItem.getSyncObjectActionMap()
-							.keySet();
-					for (SynchronizableObject synchronizableObject : keySet) {
-						Notification notification = InfomngmntFactory.eINSTANCE
-								.createNotification();
-						InformationUnitListItem item = (InformationUnitListItem) synchronizableObject;
-						IInfoType infoType = InformationExtensionManager.getInstance()
-								.getInfoTypeByType(item.getType());
-						if (infoType != null) {
-							notification.setImage(infoType.getImage());
-						}
-						notification.setTimeStamp(new Date());
-						notification.setMessage(NLS.bind("Updated \"{0}\"", item.getLabel()));
-						notification.getAffectedInfoUnitIds().add(item.getId());
-						notification.setImportance(NotificationImportance.MEDIUM);
-						// notification.setSource(synchronizableObject.getSynchronizationMetaData()
-						// .getRepositoryId());
-						notification.setSeverity(Severity.OK);
-						returnValue.getChildren().add(notification);
-					}
-				}
+				Notification synchronizeCategory = SyncUtil.synchronizeCategory((Category) eObject,
+						monitor);
+				returnValue.getChildren().addAll(synchronizeCategory.getChildren());
 			} catch (Exception e) {
-				sync.setSeverity(Severity.ERROR);
-				sync.setDetails(e.getMessage());
-				sync.setImportance(NotificationImportance.HIGH);
-				returnValue.getChildren().add(sync);
+				// we do nothing
 			}
-
 		}
 		return returnValue.getChildren();
 	}

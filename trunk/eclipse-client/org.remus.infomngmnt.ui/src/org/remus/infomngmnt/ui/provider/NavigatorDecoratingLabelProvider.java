4d004a8424cae12e63a82f04f71c2ff8f1e644bc
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2009-08-11 11:39:14 +0000

    RIMCORE-61

--- trunk/eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/provider/NavigatorDecoratingLabelProvider.java
+++ trunk/eclipse-client/org.remus.infomngmnt.ui/src/org/remus/infomngmnt/ui/provider/NavigatorDecoratingLabelProvider.java
@@ -7,15 +7,9 @@ import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.DecoratingStyledCellLabelProvider;
 import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
-import org.eclipse.jface.viewers.IColorProvider;
-import org.eclipse.jface.viewers.IFontProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.jface.viewers.ViewerColumn;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPreferenceConstants;
 import org.eclipse.ui.PlatformUI;
@@ -40,126 +34,6 @@ import org.remus.infomngmnt.common.ui.image.ResourceManager;
  */
 public class NavigatorDecoratingLabelProvider extends DecoratingStyledCellLabelProvider implements
 		IPropertyChangeListener, ILabelProvider {
-
-	private static class StyledLabelProviderAdapter implements IStyledLabelProvider,
-			IColorProvider, IFontProvider {
-
-		private final ILabelProvider provider;
-
-		public StyledLabelProviderAdapter(final ILabelProvider provider) {
-			this.provider = provider;
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @seeorg.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.
-		 * IStyledLabelProvider#getImage(java.lang.Object)
-		 */
-		public Image getImage(final Object element) {
-			return this.provider.getImage(element);
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @seeorg.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.
-		 * IStyledLabelProvider#getStyledText(java.lang.Object)
-		 */
-		public StyledString getStyledText(final Object element) {
-			if (this.provider instanceof IStyledLabelProvider) {
-				return ((IStyledLabelProvider) this.provider).getStyledText(element);
-			}
-			String text = this.provider.getText(element);
-			if (text == null) {
-				text = ""; //$NON-NLS-1$
-			}
-			return new StyledString(text);
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.jface.viewers.IBaseLabelProvider#addListener(org.eclipse
-		 * .jface.viewers.ILabelProviderListener)
-		 */
-		public void addListener(final ILabelProviderListener listener) {
-			this.provider.addListener(listener);
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see org.eclipse.jface.viewers.IBaseLabelProvider#dispose()
-		 */
-		public void dispose() {
-			this.provider.dispose();
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.jface.viewers.IBaseLabelProvider#isLabelProperty(java
-		 * .lang.Object, java.lang.String)
-		 */
-		public boolean isLabelProperty(final Object element, final String property) {
-			return this.provider.isLabelProperty(element, property);
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.jface.viewers.IBaseLabelProvider#removeListener(org.eclipse
-		 * .jface.viewers.ILabelProviderListener)
-		 */
-		public void removeListener(final ILabelProviderListener listener) {
-			this.provider.removeListener(listener);
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.jface.viewers.IColorProvider#getBackground(java.lang.
-		 * Object)
-		 */
-		public Color getBackground(final Object element) {
-			if (this.provider instanceof IColorProvider) {
-				return ((IColorProvider) this.provider).getBackground(element);
-			}
-			return null;
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.jface.viewers.IColorProvider#getForeground(java.lang.
-		 * Object)
-		 */
-		public Color getForeground(final Object element) {
-			if (this.provider instanceof IColorProvider) {
-				return ((IColorProvider) this.provider).getForeground(element);
-			}
-			return null;
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.eclipse.jface.viewers.IFontProvider#getFont(java.lang.Object)
-		 */
-		public Font getFont(final Object element) {
-			if (this.provider instanceof IFontProvider) {
-				return ((IFontProvider) this.provider).getFont(element);
-			}
-			return null;
-		}
-	}
 
 	/**
 	 * Creates a {@link NavigatorDecoratingLabelProvider}

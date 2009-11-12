72f0ee5564e348a66983310d95f350b2c63a25d5
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2009-11-12 20:48:43 +0000

    changes

---
+++ eclipse-client/org.remus.infomngmnt.birtreport/src/org/remus/infomngmnt/birtreport/ui/DateParameterControl.java
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.birtreport.ui;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+import org.remus.infomngmnt.birtreport.parameter.AbstractParameterControl;
+import org.remus.infomngmnt.common.ui.swt.DateCombo;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class DateParameterControl extends AbstractParameterControl {
+
+	public static final String DEFAULT_VALUE = "default"; //$NON-NLS-1$
+
+	public static final String INPUT_DATE_PATTERN = "pattern"; //$NON-NLS-1$
+
+	public static final String OUTPUT_DATE_PATTERN = "outputpatter"; //$NON-NLS-1$
+
+	private Date date2Set;
+
+	/**
+	 * 
+	 */
+	public DateParameterControl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.remus.infomngmnt.birtreport.parameter.AbstractParameterControl#
+	 * createPartControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createPartControl(final Composite parent) {
+		DateCombo dateCombo = new DateCombo(parent, SWT.FLAT);
+		if (this.options.get(DEFAULT_VALUE) != null) {
+			try {
+				if (this.options.get(INPUT_DATE_PATTERN) != null) {
+					this.date2Set = new SimpleDateFormat().parse(this.options.get(DEFAULT_VALUE));
+				} else {
+					this.date2Set = SimpleDateFormat.getDateInstance().parse(
+							this.options.get(DEFAULT_VALUE));
+				}
+				dateCombo.setSelection(new StructuredSelection(this.date2Set));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		dateCombo.addSelectionChangedListener(new ISelectionChangedListener() {
+			public void selectionChanged(final SelectionChangedEvent event) {
+				DateParameterControl.this.date2Set = (Date) ((IStructuredSelection) event
+						.getSelection()).getFirstElement();
+			}
+		});
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.remus.infomngmnt.birtreport.parameter.AbstractParameterControl#
+	 * getParameterValue()
+	 */
+	@Override
+	public String getParameterValue() {
+		if (this.date2Set != null) {
+			if (this.options.get(OUTPUT_DATE_PATTERN) != null) {
+				return new SimpleDateFormat(this.options.get(OUTPUT_DATE_PATTERN))
+						.format(this.date2Set);
+			} else {
+				return SimpleDateFormat.getDateInstance().format(this.date2Set);
+			}
+		}
+		return null;
+	}
+
+}
---
+++ eclipse-client/org.remus.infomngmnt.birtreport/src/org/remus/infomngmnt/birtreport/ui/ReportParameterDialog.java
@@ -0,0 +1,133 @@
+package org.remus.infomngmnt.birtreport.ui;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.StatusDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import org.remus.infomngmnt.util.StatusCreator;
+
+public class ReportParameterDialog extends StatusDialog {
+	private Text text;
+
+	String name;
+
+	String value;
+
+	private Text text2;
+
+	public final String getName() {
+		return this.name;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	/**
+	 * Create the dialog.
+	 * 
+	 * @param parentShell
+	 */
+	public ReportParameterDialog(final Shell parentShell, final String initialName,
+			final String initialValue) {
+		super(parentShell);
+		this.value = initialValue;
+		setShellStyle(getShellStyle() | SWT.RESIZE);
+		this.name = initialName;
+	}
+
+	@Override
+	protected void configureShell(final Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("Add/Edit Report parameter");
+	}
+
+	/**
+	 * Create contents of the dialog.
+	 * 
+	 * @param parent
+	 */
+	@Override
+	protected Control createDialogArea(final Composite parent) {
+		Composite container = (Composite) super.createDialogArea(parent);
+		container.setLayout(new GridLayout(2, false));
+
+		Label label = new Label(container, SWT.NONE);
+		label.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, true, 1, 1));
+		label.setText("Enter parameter name:");
+
+		this.text = new Text(container, SWT.BORDER);
+		if (this.name != null) {
+			this.text.setText(this.name);
+		}
+		this.text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
+		this.text.addListener(SWT.Modify, new Listener() {
+			public void handleEvent(final Event event) {
+				ReportParameterDialog.this.name = ReportParameterDialog.this.text.getText();
+				updateStatus(false);
+			}
+		});
+
+		Label label2 = new Label(container, SWT.NONE);
+		label2.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, true, 1, 1));
+		label2.setText("Enter parameter value:");
+
+		this.text2 = new Text(container, SWT.BORDER);
+		if (this.value != null) {
+			this.text2.setText(this.value);
+		}
+		this.text2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
+		this.text2.addListener(SWT.Modify, new Listener() {
+			public void handleEvent(final Event event) {
+				ReportParameterDialog.this.value = ReportParameterDialog.this.text2.getText();
+				updateStatus(false);
+			}
+		});
+
+		return container;
+	}
+
+	protected void updateStatus(final boolean initial) {
+		IStatus status2set = Status.OK_STATUS;
+		if (this.text.getText().trim().length() == 0) {
+			status2set = StatusCreator.newStatus("Parameter name is mandatory");
+		}
+		if (initial) {
+			updateButtonsEnableState(status2set);
+		} else {
+			updateStatus(status2set);
+		}
+
+	}
+
+	/**
+	 * Create contents of the button bar.
+	 * 
+	 * @param parent
+	 */
+	@Override
+	protected void createButtonsForButtonBar(final Composite parent) {
+		super.createButtonsForButtonBar(parent);
+		updateStatus(false);
+	}
+
+	/**
+	 * Return the initial size of the dialog.
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(450, 200);
+	}
+
+}

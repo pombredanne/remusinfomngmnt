fda17126e863a1033dca33aa74677927e39b6ead
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-02-26 14:40:25 +0000

    branching

---
+++ branches/pre_proposal/org.remus.infomngmnt.connector.folder/src/org/remus/infomngmnt/connector/folder/ui/NewFolderRepositoryWizard.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.connector.folder.ui;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.swt.widgets.Composite;
+
+import org.remus.infomngmnt.RemoteRepository;
+import org.remus.infomngmnt.ui.remote.NewRepositoryWizard;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class NewFolderRepositoryWizard extends NewRepositoryWizard {
+
+	private FolderConnectionWizardPage page1;
+
+	/**
+	 * 
+	 */
+	public NewFolderRepositoryWizard() {
+		setWindowTitle("Local Folder Repository");
+	}
+
+	@Override
+	public void addPages() {
+		addPage(this.page1);
+	}
+
+	@Override
+	public void init(final IStructuredSelection selection) {
+		super.init(selection);
+		this.page1 = new FolderConnectionWizardPage();
+	}
+
+	@Override
+	public IWizardPage getStartingPage() {
+		IWizardPage startingPage = super.getStartingPage();
+		if (startingPage == this.page1) {
+			this.page1.setRemoteObject(getRepository());
+		}
+		return startingPage;
+	}
+
+	@Override
+	public void createPageControls(final Composite pageContainer) {
+		// do nothing. no precreation.
+	}
+
+	@Override
+	protected void configureRepository(final RemoteRepository newRemoteRepositry) {
+		newRemoteRepositry.setName("local");
+	}
+
+}

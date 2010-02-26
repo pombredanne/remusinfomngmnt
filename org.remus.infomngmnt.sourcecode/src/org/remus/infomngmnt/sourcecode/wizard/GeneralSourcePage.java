4fbc4294d4df805f8b6f60c24a3dea5264a01253
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2010-02-26 15:46:13 +0000

    branch

---
+++ org.remus.infomngmnt.sourcecode/src/org/remus/infomngmnt/sourcecode/wizard/GeneralSourcePage.java
@@ -0,0 +1,117 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.sourcecode.wizard;
+
+import java.util.Collection;
+
+import org.eclipse.core.databinding.UpdateValueStrategy;
+import org.eclipse.core.databinding.observable.value.IObservableValue;
+import org.eclipse.emf.databinding.EMFObservables;
+import org.eclipse.jface.databinding.swt.ISWTObservableValue;
+import org.eclipse.jface.databinding.swt.SWTObservables;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+
+import org.remus.infomngmnt.Category;
+import org.remus.infomngmnt.InfomngmntPackage;
+import org.remus.infomngmnt.InformationUnitListItem;
+import org.remus.infomngmnt.common.ui.UIUtil;
+import org.remus.infomngmnt.common.ui.image.ResourceManager;
+import org.remus.infomngmnt.sourcecode.SourceCodePlugin;
+import org.remus.infomngmnt.ui.newwizards.GeneralPage;
+import org.remus.infomngmnt.util.InformationUtil;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class GeneralSourcePage extends GeneralPage {
+
+	protected static final String SECTION_NAME = "SECTION_NAME";
+	protected static final String SECTION_KEY = "SECTION_KEY";
+	private Combo typeCombo;
+	private Collection<String> values;
+	private IDialogSettings settings;
+
+	public GeneralSourcePage(final InformationUnitListItem selection) {
+		super(selection);
+
+	}
+
+	public GeneralSourcePage(final Category category) {
+		super(category);
+	}
+
+	@Override
+	public void createControl(final Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		container.setLayout(new GridLayout());
+
+		setTitle("New Source-Code");
+		setMessage("This wizard enables you to create a new source code snippet.");
+		setImageDescriptor(ResourceManager.getPluginImageDescriptor(SourceCodePlugin.getDefault(),
+				"icons/iconexperience/wizards/new_sourcode_wizard_title.png"));
+
+		doCreateParentElementGroup(container);
+		Group group = new Group(container, SWT.NONE);
+		group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		group.setLayout(new GridLayout(2, false));
+		group.setText("Name && Type");
+		doCreateNameElements(group);
+
+		new Label(group, SWT.NONE).setText("Type");
+		this.typeCombo = new Combo(group, SWT.READ_ONLY);
+		this.typeCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		this.values = SourceCodePlugin.getDefault().getSourceTypes().keySet();
+		for (String string : this.values) {
+			this.typeCombo.add(string);
+		}
+
+		doCreatePropertiesGroup(container);
+		initDatabinding();
+		presetValues();
+		initValidation();
+		setControl(container);
+	}
+
+	@Override
+	protected void initDatabinding() {
+		this.settings = SourceCodePlugin.getDefault().getDialogSettings();
+		super.initDatabinding();
+		ISWTObservableValue swtType = SWTObservables.observeText(this.typeCombo);
+		IObservableValue emfType = EMFObservables.observeValue(InformationUtil.getChildByType(
+				this.unit, SourceCodePlugin.SRCTYPE_NAME),
+				InfomngmntPackage.Literals.INFORMATION_UNIT__STRING_VALUE);
+		this.ctx.bindValue(swtType, emfType, new UpdateValueStrategy() {
+			@Override
+			public Object convert(final Object value) {
+				if (value != null && value.toString().length() > 0) {
+					UIUtil.getDialogSettings(SECTION_NAME, GeneralSourcePage.this.settings).put(
+							SECTION_KEY, GeneralSourcePage.this.typeCombo.getSelectionIndex());
+				}
+				return super.convert(value);
+			}
+		}, null);
+		try {
+			this.typeCombo.select(UIUtil.getDialogSettings(SECTION_NAME, this.settings).getInt(
+					SECTION_KEY));
+		} catch (NumberFormatException e) {
+			this.typeCombo.select(0);
+		}
+	}
+}
---
+++ org.remus.infomngmnt.sourcecode/src/org/remus/infomngmnt/sourcecode/wizard/NewSourceCodeWizard.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.sourcecode.wizard;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+
+import org.remus.infomngmnt.Category;
+import org.remus.infomngmnt.InformationUnit;
+import org.remus.infomngmnt.InformationUnitListItem;
+import org.remus.infomngmnt.RuleValue;
+import org.remus.infomngmnt.core.extension.TransferWrapper;
+import org.remus.infomngmnt.sourcecode.SourceCodePlugin;
+import org.remus.infomngmnt.sourcecode.prefs.SourceCodeRulePreferencePage;
+import org.remus.infomngmnt.ui.extension.AbstractCreationPreferencePage;
+import org.remus.infomngmnt.ui.newwizards.NewInfoObjectWizard;
+import org.remus.infomngmnt.util.InformationUtil;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class NewSourceCodeWizard extends NewInfoObjectWizard {
+
+	/**
+	 * 
+	 */
+	public NewSourceCodeWizard() {
+		setWindowTitle("New Source Code");
+	}
+
+	@Override
+	public void setDefaults(final Object value, final RuleValue ruleValue,
+			final TransferWrapper transferType) {
+		this.newElement.setStringValue(String.valueOf(value));
+		InformationUnit childByType = InformationUtil.getChildByType(ruleValue,
+				AbstractCreationPreferencePage.NODENAME_PREDEFINED_CATEGORY);
+		if (childByType != null && childByType.getStringValue() != null) {
+			this.page1.setCategoryString(childByType.getStringValue());
+		}
+		childByType = InformationUtil.getChildByType(ruleValue,
+				SourceCodeRulePreferencePage.NODENAME_PREDEFINED_SOURCETYPE);
+		if (childByType != null && childByType.getStringValue() != null) {
+			InformationUtil.getChildByType(this.newElement, SourceCodePlugin.SRCTYPE_NAME)
+					.setStringValue(childByType.getStringValue());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
+	 * org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	@Override
+	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
+		Object firstElement = selection.getFirstElement();
+		if (firstElement instanceof Category) {
+			this.page1 = new GeneralSourcePage((Category) firstElement);
+		} else if (firstElement instanceof InformationUnitListItem) {
+			this.page1 = new GeneralSourcePage((InformationUnitListItem) firstElement);
+		} else {
+			this.page1 = new GeneralSourcePage((Category) null);
+		}
+		setCategoryToPage();
+
+	}
+
+	@Override
+	protected String getInfoTypeId() {
+		return SourceCodePlugin.TYPE_ID;
+	}
+
+}

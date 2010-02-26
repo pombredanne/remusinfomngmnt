6f1366cdc9523634b3a8fe89819779bfcc671527
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-02-26 14:51:50 +0000

    branching

---
+++ branches/pre_proposal/org.remus.infomngmnt.mail/src/org/remus/infomngmnt/mail/ui/MailPreferencePage.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.mail.ui;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class MailPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	/**
+	 * @wbp.parser.constructor
+	 * 
+	 */
+	public MailPreferencePage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param title
+	 */
+	public MailPreferencePage(final String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param title
+	 * @param image
+	 */
+	public MailPreferencePage(final String title, final ImageDescriptor image) {
+		super(title, image);
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.preference.PreferencePage#createContents(org.eclipse
+	 * .swt.widgets.Composite)
+	 */
+	@Override
+	protected Control createContents(final Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(final IWorkbench workbench) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

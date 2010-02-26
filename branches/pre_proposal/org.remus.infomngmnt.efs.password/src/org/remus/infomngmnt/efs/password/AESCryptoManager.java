909f702b75cacf11602a065cbbfa1cdc13ade529
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-02-26 14:49:11 +0000

    branching

---
+++ branches/pre_proposal/org.remus.infomngmnt.efs.password/src/org/remus/infomngmnt/efs/password/AESCryptoManager.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.efs.password;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class AESCryptoManager {
+
+	public static SecureRandom rand = new SecureRandom();
+
+	private final byte[] pass;
+
+	private final static String CIPHER_TYPE = "AES/CBC/PKCS5Padding";
+
+	private final static String CIPHER_KEY_TYPE = "AES";
+
+	private static final String HASH_ALG = "SHA-1";
+
+	private static final int KEY_LENGTH = 16;
+
+	// The salt used for symmetric key generation
+	private static final byte[] SALT = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+	// The initialization Vector for CBC Mode
+	private static final byte[] IV = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+	public AESCryptoManager(final byte[] pass) {
+		this.pass = pass;
+	}
+
+	public OutputStream getEncryptedOutputStream(final File file) throws Exception {
+		FileOutputStream fileOut = new FileOutputStream(file);
+		Cipher cipher = Cipher.getInstance(CIPHER_TYPE);
+		IvParameterSpec ivParamSpec = new IvParameterSpec(IV);
+		SecretKeySpec sKeySpec = new SecretKeySpec(deriveKey(), CIPHER_KEY_TYPE);
+		cipher.init(Cipher.ENCRYPT_MODE, sKeySpec, ivParamSpec);
+
+		return new CipherOutputStream(fileOut, cipher);
+	}
+
+	public InputStream getDecryptedInputStream(final File file) throws Exception {
+
+		FileInputStream fileIn = new FileInputStream(file);
+		IvParameterSpec ivParamSpec = new IvParameterSpec(IV);
+		SecretKeySpec sKeySpec = new SecretKeySpec(deriveKey(), CIPHER_KEY_TYPE);
+		Cipher cipher = Cipher.getInstance(CIPHER_TYPE);
+		cipher.init(Cipher.DECRYPT_MODE, sKeySpec, ivParamSpec);
+
+		return new CipherInputStream(fileIn, cipher);
+	}
+
+	/*
+	 * Derives the file specific session key from a supplied master secret
+	 */
+	private byte[] deriveKey() throws Exception {
+
+		// The digest derives a key from the master secret and the salt
+		MessageDigest md = MessageDigest.getInstance(HASH_ALG);
+		md.update(this.pass);
+		md.update(SALT);
+
+		// The derived key may be too long so we copy only the needed part
+		byte[] key = new byte[KEY_LENGTH];
+		System.arraycopy(md.digest(), 0, key, 0, KEY_LENGTH);
+
+		return key;
+	}
+
+}
---
+++ branches/pre_proposal/org.remus.infomngmnt.efs.password/src/org/remus/infomngmnt/efs/password/ui/PasswordPrompt.java
@@ -0,0 +1,114 @@
+package org.remus.infomngmnt.efs.password.ui;
+
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.StatusDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.widgets.TableWrapData;
+import org.eclipse.ui.forms.widgets.TableWrapLayout;
+
+import org.remus.infomngmnt.util.StatusCreator;
+
+public class PasswordPrompt extends StatusDialog {
+
+	private Text text;
+
+	private String pwd;
+
+	private final boolean creation;
+
+	/**
+	 * Create the dialog
+	 * 
+	 * @param parentShell
+	 */
+	public PasswordPrompt(final Shell parentShell, final boolean creation) {
+		super(parentShell);
+		this.creation = creation;
+	}
+
+	/**
+	 * Create contents of the dialog
+	 * 
+	 * @param parent
+	 */
+	@Override
+	protected Control createDialogArea(final Composite parent) {
+		Composite container = (Composite) super.createDialogArea(parent);
+		container.setLayout(new TableWrapLayout());
+
+		final Label pleaseEnterALabel = new Label(container, SWT.WRAP);
+		if (this.creation) {
+			pleaseEnterALabel
+					.setText("Please enter a password which is used for en- and decryption of project contents. Pleas keep this password in mind, it won't be saved on disc.");
+		} else {
+			pleaseEnterALabel
+					.setText("Please enter the password you have chosen on creating the projects. This is required to access the content of the project.");
+		}
+
+		final Label label_1 = new Label(container, SWT.SEPARATOR | SWT.HORIZONTAL);
+		label_1.setLayoutData(new TableWrapData(TableWrapData.FILL, TableWrapData.TOP));
+
+		final Label enterYourPasswordLabel = new Label(container, SWT.NONE);
+		enterYourPasswordLabel.setText("Enter your password.");
+
+		this.text = new Text(container, SWT.BORDER | SWT.PASSWORD);
+		final TableWrapData twd_text = new TableWrapData(TableWrapData.LEFT, TableWrapData.TOP);
+		twd_text.align = TableWrapData.FILL;
+		this.text.setLayoutData(twd_text);
+		this.text.addListener(SWT.Modify, new Listener() {
+			public void handleEvent(final Event event) {
+				if (((Text) event.widget).getText().trim().length() == 0) {
+					updateStatus(StatusCreator.newStatus("No empty password allowed"));
+				} else {
+					updateStatus(Status.OK_STATUS);
+				}
+				PasswordPrompt.this.pwd = ((Text) event.widget).getText();
+			}
+		});
+
+		return container;
+	}
+
+	/**
+	 * Create contents of the button bar
+	 * 
+	 * @param parent
+	 */
+	@Override
+	protected void createButtonsForButtonBar(final Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+		updateStatus(StatusCreator.newStatus("Nothing selected"));
+	}
+
+	/**
+	 * Return the initial size of the dialog
+	 */
+	@Override
+	protected Point getInitialSize() {
+		return new Point(300, 205);
+	}
+
+	@Override
+	protected void configureShell(final Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("Enter password");
+	}
+
+	/**
+	 * @return the pwd
+	 */
+	public String getPwd() {
+		return this.pwd;
+	}
+
+}

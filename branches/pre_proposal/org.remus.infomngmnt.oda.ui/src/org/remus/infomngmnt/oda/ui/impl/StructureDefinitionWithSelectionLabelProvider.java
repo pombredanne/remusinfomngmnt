7f6351e22976e3d2b4ed71ed262c14ea3fab3bb8
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-02-26 15:15:09 +0000

    branching

---
+++ branches/pre_proposal/org.remus.infomngmnt.oda.ui/src/org/remus/infomngmnt/oda/ui/impl/StructureDefinitionWithSelectionLabelProvider.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.oda.ui.impl;
+
+import org.eclipse.jface.viewers.IFontProvider;
+import org.eclipse.swt.graphics.Font;
+
+import org.remus.infomngmnt.InformationStructureItem;
+import org.remus.infomngmnt.common.ui.image.ResourceManager;
+import org.remus.infomngmnt.ui.structuredefinition.StructureDefinitionLabelProvider;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class StructureDefinitionWithSelectionLabelProvider extends StructureDefinitionLabelProvider
+		implements IFontProvider {
+
+	private final Font defaultFont;
+
+	public StructureDefinitionWithSelectionLabelProvider(final Font defaultFont) {
+		this.defaultFont = defaultFont;
+	}
+
+	private String selection;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.viewers.IFontProvider#getFont(java.lang.Object)
+	 */
+	public Font getFont(final Object arg0) {
+		if (((InformationStructureItem) arg0).getId().equals(this.selection)) {
+			return ResourceManager.getBoldFont(this.defaultFont);
+		}
+		return null;
+	}
+
+	/**
+	 * @param selection
+	 *            the selection to set
+	 */
+	public final void setSelection(final String selection) {
+		this.selection = selection;
+	}
+
+}

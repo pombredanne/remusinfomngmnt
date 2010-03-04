ff26de68c3e61496549960dcc679ef6af9d6835c
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-03-04 00:23:33 +0000

    changes

--- trunk/eclipse-client/org.remus.infomngmnt.search.local/src/org/remus/infomngmnt/search/local/Activator.java
+++ trunk/eclipse-client/org.remus.infomngmnt.search.local/src/org/remus/infomngmnt/search/local/Activator.java
@@ -1,14 +1,12 @@
 package org.remus.infomngmnt.search.local;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
-
-import org.remus.infomngmnt.search.service.ILuceneCustomizer;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends Plugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.remus.infomngmnt.search.local"; //$NON-NLS-1$
@@ -24,28 +22,33 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	@Override
-	public void start(BundleContext context) throws Exception {
+	public void start(final BundleContext context) throws Exception {
 		super.start(context);
-		context.registerService(ILuceneCustomizer.class.getName(), new DefaultLuceneCustomizer(), null);
 		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	@Override
-	public void stop(BundleContext context) throws Exception {
+	public void stop(final BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {

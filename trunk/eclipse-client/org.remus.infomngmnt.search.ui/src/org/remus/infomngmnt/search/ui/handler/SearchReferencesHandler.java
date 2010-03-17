07f063f3ada19c96a877cfb5909f482bedcffc8f
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-03-17 22:47:24 +0000

    minor changes

--- trunk/eclipse-client/org.remus.infomngmnt.search.ui/src/org/remus/infomngmnt/search/ui/handler/SearchReferencesHandler.java
+++ trunk/eclipse-client/org.remus.infomngmnt.search.ui/src/org/remus/infomngmnt/search/ui/handler/SearchReferencesHandler.java
@@ -16,8 +16,13 @@ import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.ui.URIEditorInput;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import org.remus.infomngmnt.AbstractInformationUnit;
@@ -25,8 +30,11 @@ import org.remus.infomngmnt.core.services.IReferencedUnitStore;
 import org.remus.infomngmnt.search.Search;
 import org.remus.infomngmnt.search.SearchFactory;
 import org.remus.infomngmnt.search.service.ILuceneCustomizer;
+import org.remus.infomngmnt.search.service.ISearchCallBack;
 import org.remus.infomngmnt.search.service.ISearchService;
 import org.remus.infomngmnt.search.ui.SearchUIActivator;
+import org.remus.infomngmnt.search.ui.editor.SearchResultEditor;
+import org.remus.infomngmnt.util.StatusCreator;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
@@ -49,13 +57,41 @@ public class SearchReferencesHandler extends AbstractHandler {
 						.getService(IReferencedUnitStore.class);
 				String[] referencedInfoUnitIds = service
 						.getReferencedInfoUnitIds(((AbstractInformationUnit) firstElement).getId());
-				Search createSearch = SearchFactory.eINSTANCE.createSearch();
+				final Search createSearch = SearchFactory.eINSTANCE.createSearch();
 				createSearch.setIdSearch(true);
 				createSearch.setSearchString(StringUtils.join(referencedInfoUnitIds,
 						ILuceneCustomizer.ID_SEPARATOR));
 				ISearchService searchService = SearchUIActivator.getDefault().getServiceTracker()
 						.getService(ISearchService.class);
-				searchService.search(createSearch, true);
+				searchService.search(createSearch, true, new ISearchCallBack() {
+
+					public void beforeSearch(final IProgressMonitor monitor, final Search search) {
+						// TODO Auto-generated method stub
+
+					}
+
+					public void afterSearch(final IProgressMonitor monitor, final Search search) {
+						Display.getDefault().asyncExec(new Runnable() {
+							public void run() {
+								try {
+									SearchUIActivator.getDefault().getSearchContext().deactivate();
+									PlatformUI
+											.getWorkbench()
+											.getActiveWorkbenchWindow()
+											.getActivePage()
+											.openEditor(
+													new URIEditorInput(search.eResource().getURI()),
+													SearchResultEditor.class.getName());
+								} catch (PartInitException e) {
+									SearchUIActivator.getDefault().getLog().log(
+											StatusCreator.newStatus("Error opening search-result",
+													e));
+								}
+							}
+						});
+
+					}
+				});
 			}
 		}
 		return null;

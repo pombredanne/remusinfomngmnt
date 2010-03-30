6942bf334d7f8b72cfa11c2438351f093405cdf5
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2010-03-30 07:25:13 +0000

    RIMCORE-97

--- eclipse-client/org.remus.infomngmnt.ui.desktop/src/org/remus/infomngmnt/ui/desktop/internal/services/ToolbarContributionExtensionService.java
+++ eclipse-client/org.remus.infomngmnt.ui.desktop/src/org/remus/infomngmnt/ui/desktop/internal/services/ToolbarContributionExtensionService.java
@@ -20,6 +20,7 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.Platform;
 
+import org.remus.infomngmnt.common.core.extension.PluginRegistryDynamic;
 import org.remus.infomngmnt.ui.desktop.extension.IToolbarContribution;
 import org.remus.infomngmnt.ui.desktop.internal.tray.ToolbarContribution;
 import org.remus.infomngmnt.ui.desktop.services.IToolbarContributionService;
@@ -28,7 +29,8 @@ import org.remus.uimodel.provider.UimodelEditPlugin;
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
  */
-public class ToolbarContributionExtensionService implements IToolbarContributionService {
+public class ToolbarContributionExtensionService extends PluginRegistryDynamic implements
+		IToolbarContributionService {
 
 	public static final String EXTENSION_POINT = UimodelEditPlugin.Implementation.PLUGIN_ID
 			+ ".trayAction"; //$NON-NLS-1$
@@ -43,11 +45,13 @@ public class ToolbarContributionExtensionService implements IToolbarContribution
 
 	private Map<String, IToolbarContribution> items;
 
-	ToolbarContributionExtensionService() {
-		init();
+	public ToolbarContributionExtensionService() {
+		super(EXTENSION_POINT);
+
 	}
 
-	private void init() {
+	@Override
+	protected void init() {
 		this.items = new HashMap<String, IToolbarContribution>();
 		final IExtensionPoint extensionPoint = Platform.getExtensionRegistry().getExtensionPoint(
 				EXTENSION_POINT);
@@ -63,17 +67,27 @@ public class ToolbarContributionExtensionService implements IToolbarContribution
 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.remus.infomngmnt.ui.desktop.internal.services.IToolbarContributionService#getAllContributions()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.remus.infomngmnt.ui.desktop.internal.services.IToolbarContributionService
+	 * #getAllContributions()
 	 */
 	public Collection<IToolbarContribution> getAllContributions() {
+		checkForInitialization();
 		return this.items.values();
 	}
 
-	/* (non-Javadoc)
-	 * @see org.remus.infomngmnt.ui.desktop.internal.services.IToolbarContributionService#getContributionById(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.remus.infomngmnt.ui.desktop.internal.services.IToolbarContributionService
+	 * #getContributionById(java.lang.String)
 	 */
 	public IToolbarContribution getContributionById(final String id) {
+		checkForInitialization();
 		return this.items.get(id);
 	}
 }

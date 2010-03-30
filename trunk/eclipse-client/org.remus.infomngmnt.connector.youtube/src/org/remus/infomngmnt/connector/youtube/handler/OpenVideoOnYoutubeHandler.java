1f93e4a1e1c02c5472f544b2967d6fd5fb2ebab9
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-03-30 11:28:01 +0000

    RIMCONNECTORS-22

---
+++ trunk/eclipse-client/org.remus.infomngmnt.connector.youtube/src/org/remus/infomngmnt/connector/youtube/handler/OpenVideoOnYoutubeHandler.java
@@ -0,0 +1,101 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Tom Seidel, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Tom Seidel - initial API and implementation
+ *******************************************************************************/
+
+package org.remus.infomngmnt.connector.youtube.handler;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.swt.program.Program;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import org.remus.infomngmnt.InformationUnit;
+import org.remus.infomngmnt.InformationUnitListItem;
+import org.remus.infomngmnt.RemoteObject;
+import org.remus.infomngmnt.core.remote.RemoteException;
+import org.remus.infomngmnt.core.remote.sync.SyncUtil;
+import org.remus.infomngmnt.model.remote.IRepository;
+import org.remus.infomngmnt.ui.handlerutil.CopyToClipboardHandler;
+import org.remus.infomngmnt.ui.handlerutil.InformationHandlerUtil;
+import org.remus.infomngmnt.ui.util.CancelableRunnable;
+import org.remus.infomngmnt.util.StatusCreator;
+
+import com.google.gdata.data.youtube.VideoEntry;
+
+/**
+ * @author Tom Seidel <tom.seidel@remus-software.org>
+ */
+public class OpenVideoOnYoutubeHandler extends CopyToClipboardHandler {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.
+	 * ExecutionEvent)
+	 */
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		InformationUnit infoUnit = InformationHandlerUtil
+				.getInformationUnitFromExecutionEvent(event);
+		if (infoUnit != null) {
+			final InformationUnitListItem adapter = (InformationUnitListItem) infoUnit
+					.getAdapter(InformationUnitListItem.class);
+			final IRepository remoteRepository = SyncUtil
+					.getRepositoryImplemenationByRepositoryId(adapter.getSynchronizationMetaData()
+							.getRepositoryId());
+			if (remoteRepository != null) {
+				CancelableRunnable runnable = new CancelableRunnable() {
+
+					@Override
+					protected IStatus runCancelableRunnable(final IProgressMonitor monitor) {
+						monitor
+								.beginTask("Contacting Youtube Repository",
+										IProgressMonitor.UNKNOWN);
+						RemoteObject remoteObject;
+						try {
+							remoteObject = remoteRepository.getRemoteObjectBySynchronizableObject(
+									adapter, monitor);
+						} catch (RemoteException e) {
+							return StatusCreator
+									.newStatus("Error contacting Youtube Repository", e);
+						}
+						if (remoteObject != null
+								&& remoteObject.getWrappedObject() instanceof VideoEntry) {
+							VideoEntry wrappedObject = (VideoEntry) remoteObject.getWrappedObject();
+							Program.launch(wrappedObject.getHtmlLink().getHref());
+						}
+						return Status.OK_STATUS;
+					}
+				};
+				ProgressMonitorDialog pmd = new ProgressMonitorDialog(HandlerUtil
+						.getActiveShell(event));
+				try {
+					pmd.run(true, true, runnable);
+				} catch (Exception e) {
+					ErrorDialog.openError(HandlerUtil.getActiveShell(event), "Error",
+							"Error contacting Flickr",
+							e.getCause() instanceof CoreException ? ((CoreException) e.getCause())
+									.getStatus() : null);
+				}
+
+			}
+		}
+
+		return null;
+	}
+
+}

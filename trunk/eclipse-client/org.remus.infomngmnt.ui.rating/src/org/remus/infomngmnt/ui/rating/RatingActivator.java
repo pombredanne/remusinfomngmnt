656948b6169d22a85aee6aeef84415b2b7fa3dce
deasw <deasw@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-04-23 08:34:08 +0000

    RIMCORE-89: not finished

---
+++ trunk/eclipse-client/org.remus.infomngmnt.ui.rating/src/org/remus/infomngmnt/ui/rating/RatingActivator.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Andreas Deinlein, Remus Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *
+ * Contributors:
+ *     Andreas Deinlein - initial version
+ *******************************************************************************/
+
+package org.remus.infomngmnt.ui.rating;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class RatingActivator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.remus.infomngmnt.ui.rating";
+
+	// The shared instance
+	private static RatingActivator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public RatingActivator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static RatingActivator getDefault() {
+		return plugin;
+	}
+
+}

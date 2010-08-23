c797ec10d43fb1dba75d20c2d9b198a4a3246e8a
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2010-08-23 22:36:58 +0000

    change to new eclipse naming structure

--- trunk/eclipse-client/org.remus.infomngmnt.video/src/org/remus/infomngmnt/video/newwizard/VideoCreationWizard.java
+++ trunk/eclipse-client/org.remus.infomngmnt.video/src/org/remus/infomngmnt/video/newwizard/VideoCreationWizard.java
@@ -17,15 +17,14 @@ import java.lang.reflect.InvocationTargetException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
-
-import org.remus.infomngmnt.common.ui.UIUtil;
-import org.remus.infomngmnt.ui.newwizards.NewInfoObjectWizard;
-import org.remus.infomngmnt.ui.operation.LoadFileToTmpFromPathRunnable;
-import org.remus.infomngmnt.ui.rules.internal.transfer.FileTransferWrapper;
-import org.remus.infomngmnt.ui.rules.service.ICreationTrigger;
-import org.remus.infomngmnt.ui.rules.transfer.TransferWrapper;
-import org.remus.infomngmnt.ui.rules.wizard.NewObjectWizardDelegate;
-import org.remus.rules.RuleValue;
+import org.eclipse.remus.common.ui.UIUtil;
+import org.eclipse.remus.rules.RuleValue;
+import org.eclipse.remus.ui.newwizards.NewInfoObjectWizard;
+import org.eclipse.remus.ui.operation.LoadFileToTmpFromPathRunnable;
+import org.eclipse.remus.ui.rules.internal.transfer.FileTransferWrapper;
+import org.eclipse.remus.ui.rules.service.ICreationTrigger;
+import org.eclipse.remus.ui.rules.transfer.TransferWrapper;
+import org.eclipse.remus.ui.rules.wizard.NewObjectWizardDelegate;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>

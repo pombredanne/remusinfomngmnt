62e6337addf29ae849ac366207240bfdae2c059a
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2011-01-23 18:36:17 +0000

    RESOLVED - bug 335107: [Rules] Make Rules exportable/importable https://bugs.eclipse.org/bugs/show_bug.cgi?id=335107

--- eclipse-client/org.remus.infomngmnt.groovy/src/org/remus/infomngmnt/groovy/internal/GroovyEvaluation.java
+++ eclipse-client/org.remus.infomngmnt.groovy/src/org/remus/infomngmnt/groovy/internal/GroovyEvaluation.java
@@ -11,20 +11,32 @@
  *******************************************************************************/
 package org.remus.infomngmnt.groovy.internal;
 
-import groovy.lang.GroovyShell;
+import groovy.lang.Binding;
+import groovy.lang.GroovyClassLoader;
+import groovy.util.GroovyScriptEngine;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.remus.rules.provider.RuleEditPlugin;
 import org.eclipse.remus.ui.rules.execution.IGroovyEvaluation;
 import org.eclipse.remus.ui.rules.execution.IGroovyEvaluationBinding;
+import org.eclipse.remus.ui.rules.extension.IGroovyScript;
+import org.eclipse.remus.ui.rules.processing.GroovyClassloader;
+
+import org.apache.commons.io.FileUtils;
 
 /**
  * @author Tom Seidel <tom.seidel@remus-software.org>
  */
 public class GroovyEvaluation implements IGroovyEvaluation {
 
-	private GroovyShell groovyShell;
+	private IGroovyEvaluationBinding binding;
+	private ClassLoader classLoader;
 
 	/*
 	 * (non-Javadoc)
@@ -34,11 +46,7 @@ public class GroovyEvaluation implements IGroovyEvaluation {
 	 * .eclipse.remus.ui.rules.execution.IGroovyEvaluationBinding)
 	 */
 	public void setBinding(IGroovyEvaluationBinding binding) {
-		Map<String, ? extends Object> variables = binding.getVariables();
-		Set<String> keySet = variables.keySet();
-		for (String string : keySet) {
-			getShell().setVariable(string, variables.get(string));
-		}
+		this.binding = binding;
 
 	}
 
@@ -59,15 +67,61 @@ public class GroovyEvaluation implements IGroovyEvaluation {
 	 * org.eclipse.remus.ui.rules.execution.IGroovyEvaluation#evaluate(java.
 	 * lang.String)
 	 */
-	public Object evaluate(String script) {
-		return getShell().evaluate(script);
+	public Object evaluate(String script) throws Exception {
+
+		Map<String, ? extends Object> variables = binding.getVariables();
+		Set<String> keySet = variables.keySet();
+		IPath cpPath = RuleEditPlugin.getPlugin().getStateLocation()
+				.append(IGroovyScript.CPFOLDERNAME);
+		Object evaluate = null;
+		String scriptName = "S" + System.currentTimeMillis() + ".groovy";
+		File[] listFiles = cpPath.toFile().listFiles();
+		File scriptFile = cpPath.append(scriptName).toFile();
+
+		FileUtils.writeStringToFile(scriptFile, script);
+		URL[] classPathUrls = new URL[listFiles.length + 1];
+		for (int i = 0; i < listFiles.length; i++) {
+			try {
+				classPathUrls[i] = listFiles[i].toURI().toURL();
+			} catch (MalformedURLException e) {
+			}
+		}
+		try {
+			classPathUrls[listFiles.length] = scriptFile.toURI().toURL();
+		} catch (MalformedURLException e1) {
+
+		}
+		GroovyScriptEngine engine;
+		if (classLoader == null) {
+			engine = new GroovyScriptEngine(classPathUrls);
+		} else {
+
+			engine = new GroovyScriptEngine(classPathUrls, classLoader);
+		}
+
+		Binding binding = new Binding();
+		for (String string : keySet) {
+			binding.setVariable(string, variables.get(string));
+		}
+		evaluate = engine.run(scriptFile.toURI().toString(), binding);
+		for (String string : keySet) {
+			Object variable = binding.getVariable(string);
+			this.binding.setVariable(string, variable);
+		}
+		scriptFile.deleteOnExit();
+
+		return evaluate;
 	}
 
-	private GroovyShell getShell() {
-		if (groovyShell == null) {
-			groovyShell = new GroovyShell();
+	public void setClassLoader(ClassLoader classloader) {
+		if (classloader instanceof org.eclipse.remus.ui.rules.processing.GroovyClassloader) {
+			((GroovyClassloader) classloader)
+					.addClassLoader(new GroovyClassLoader(Thread
+							.currentThread().getContextClassLoader()));
+
 		}
-		return groovyShell;
+		classLoader = classloader;
+
 	}
 
 }

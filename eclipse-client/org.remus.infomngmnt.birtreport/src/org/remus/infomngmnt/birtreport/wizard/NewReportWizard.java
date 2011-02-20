fcb4f12fb105d8cc0252b1110df089681bd6f047
Tom Seidel <192797+tmseidel@users.noreply.github.com>
2011-02-20 23:43:07 +0000

    + minor additions

--- eclipse-client/org.remus.infomngmnt.birtreport/src/org/remus/infomngmnt/birtreport/wizard/NewReportWizard.java
+++ eclipse-client/org.remus.infomngmnt.birtreport/src/org/remus/infomngmnt/birtreport/wizard/NewReportWizard.java
@@ -28,16 +28,11 @@ import org.eclipse.remus.InformationUnit;
 import org.eclipse.remus.InformationUnitListItem;
 import org.eclipse.remus.common.core.streams.StreamCloser;
 import org.eclipse.remus.common.core.util.ResourceUtil;
-import org.eclipse.remus.common.ui.UIUtil;
 import org.eclipse.remus.core.commands.CommandFactory;
 import org.eclipse.remus.core.model.InformationStructureEdit;
 import org.eclipse.remus.core.services.IEditingHandler;
-import org.eclipse.remus.ui.editors.InformationEditor;
-import org.eclipse.remus.ui.editors.InformationEditorInput;
 import org.eclipse.remus.ui.newwizards.NewInfoObjectWizard;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.PlatformUI;
-
 import org.remus.infomngmnt.birtreport.ReportActivator;
 import org.remus.infomngmnt.birtreport.extension.IReportTemplate;
 
@@ -65,14 +60,16 @@ public class NewReportWizard extends NewInfoObjectWizard {
 	 * org.eclipse.jface.viewers.IStructuredSelection)
 	 */
 	@Override
-	public void init(final IWorkbench workbench, final IStructuredSelection selection) {
-		this.service = ReportActivator.getDefault().getServiceTracker().getService(
-				IEditingHandler.class);
+	public void init(final IWorkbench workbench,
+			final IStructuredSelection selection) {
+		this.service = ReportActivator.getDefault().getServiceTracker()
+				.getService(IEditingHandler.class);
 		Object firstElement = selection.getFirstElement();
 		if (firstElement instanceof Category) {
 			this.page1 = new GeneralReportPage((Category) firstElement);
 		} else if (firstElement instanceof InformationUnitListItem) {
-			this.page1 = new GeneralReportPage((InformationUnitListItem) firstElement);
+			this.page1 = new GeneralReportPage(
+					(InformationUnitListItem) firstElement);
 		} else {
 			this.page1 = new GeneralReportPage((Category) null);
 		}
@@ -87,10 +84,12 @@ public class NewReportWizard extends NewInfoObjectWizard {
 
 	@Override
 	public IWizardPage getNextPage(final IWizardPage page) {
-		if (page == this.page1 && ((GeneralReportPage) this.page1).isTemplateSelected()) {
+		if (page == this.page1
+				&& ((GeneralReportPage) this.page1).isTemplateSelected()) {
 			this.page2
 					.setTemplate((IReportTemplate) ((IStructuredSelection) ((GeneralReportPage) this.page1)
-							.getTemplateViewer().getSelection()).getFirstElement());
+							.getTemplateViewer().getSelection())
+							.getFirstElement());
 			return super.getNextPage(page);
 		}
 		return null;
@@ -115,7 +114,8 @@ public class NewReportWizard extends NewInfoObjectWizard {
 
 	@Override
 	public boolean performFinish() {
-		this.templateSelected = ((GeneralReportPage) this.page1).isTemplateSelected();
+		this.templateSelected = ((GeneralReportPage) this.page1)
+				.isTemplateSelected();
 		if (this.templateSelected) {
 			this.selectedTemplage = this.page2.getTemplate();
 		}
@@ -129,7 +129,8 @@ public class NewReportWizard extends NewInfoObjectWizard {
 			InputStream createStream;
 			try {
 				createStream = this.selectedTemplage.createStream();
-				createTempFile.setContents(createStream, true, false, new NullProgressMonitor());
+				createTempFile.setContents(createStream, true, false,
+						new NullProgressMonitor());
 				StreamCloser.closeStreams(createStream);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -138,19 +139,20 @@ public class NewReportWizard extends NewInfoObjectWizard {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			Map<String, String> parameterValues = this.page2.getParameterValues();
+			Map<String, String> parameterValues = this.page2
+					.getParameterValues();
 			Set<String> keySet = parameterValues.keySet();
 			InformationStructureEdit edit = InformationStructureEdit
 					.newSession(ReportActivator.INFOTYPE_ID);
 			for (String string : keySet) {
-				InformationUnit params = edit.createSubType(ReportActivator.NODE_NAME_PARAM_NAME,
-						string);
-				edit.setValue(params, ReportActivator.NODE_NAME_PARAM_VALUE, parameterValues
-						.get(string));
+				InformationUnit params = edit.createSubType(
+						ReportActivator.NODE_NAME_PARAM_NAME, string);
+				edit.setValue(params, ReportActivator.NODE_NAME_PARAM_VALUE,
+						parameterValues.get(string));
 				edit.addDynamicNode(this.newElement, params, null);
 			}
-			return CommandFactory.addFileToInfoUnit(createTempFile, this.newElement, this.service
-					.getNavigationEditingDomain());
+			return CommandFactory.addFileToInfoUnit(createTempFile,
+					this.newElement, this.service.getNavigationEditingDomain());
 
 		}
 		return super.getAdditionalCommands();
@@ -158,25 +160,9 @@ public class NewReportWizard extends NewInfoObjectWizard {
 
 	@Override
 	public void dispose() {
-		ReportActivator.getDefault().getServiceTracker().ungetService(this.service);
+		ReportActivator.getDefault().getServiceTracker()
+				.ungetService(this.service);
 		super.dispose();
-	}
-
-	@Override
-	protected void performActionAfterCreation() {
-		try {
-			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().openEditor(
-					new InformationEditorInput((IFile) this.newElement.getAdapter(IFile.class)),
-					InformationEditor.ID);
-		} catch (Exception e) {
-			// will come soon.
-		}
-		// we also reveal the created list-item, that can be found in the
-		// navigation
-		UIUtil.selectAndReveal(this.newElement.getAdapter(InformationUnitListItem.class),
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-		UIUtil.selectAndReveal(this.newElement, PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow());
 	}
 
 }

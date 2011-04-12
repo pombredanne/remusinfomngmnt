dd790707e84619dab91f6d26b9eb5bd6c4c1a652
tmseidel <tmseidel@a7f20bc3-6850-0410-b2fb-b0af445374bf>
2011-04-12 14:22:28 +0000

    i18n

--- trunk/eclipse-client/org.remus.infomngmnt.richtext/src/org/remus/infomngmnt/richtext/RichTextPlugin.java
+++ trunk/eclipse-client/org.remus.infomngmnt.richtext/src/org/remus/infomngmnt/richtext/RichTextPlugin.java
@@ -9,13 +9,13 @@ import org.osgi.framework.BundleContext;
 public class RichTextPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.remus.infomngmnt.richtext";
+	public static final String PLUGIN_ID = "org.remus.infomngmnt.richtext"; //$NON-NLS-1$
 
 	// The shared instance
 	private static RichTextPlugin plugin;
-	
+
 	public static final String TYPE_ID = "HTML"; //$NON-NLS-1$
-	
+
 	/**
 	 * The constructor
 	 */
@@ -24,7 +24,10 @@ public class RichTextPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	@Override
 	public void start(final BundleContext context) throws Exception {
@@ -34,7 +37,10 @@ public class RichTextPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	@Override
 	public void stop(final BundleContext context) throws Exception {
@@ -44,7 +50,7 @@ public class RichTextPlugin extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static RichTextPlugin getDefault() {
